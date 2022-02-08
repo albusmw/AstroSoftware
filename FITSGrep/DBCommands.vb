500b2fb0fb7e8d9bf5caefe0f336212a00ca31ed
Martin Weiss <albusmw@gmx.de>
2022-02-08 11:48:06 +0100

    add FITSGrep project

---
+++ FITSGrep/DBCommands.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+Option Strict On
+
+'''<summary>Commands for the database.</summary>
+Public Class DBCommands
+
+    '''<summary>Command to build the base table.</summary>
+    '''<remarks>The most inportant informations are on-top</remarks>
+    Public Shared Function CMD_BuildTable(ByVal TableName As String, ByVal FITSKeywords As List(Of String)) As String
+
+        Dim StdType As String = "STRING DEFAULT NULL"
+        Dim SQLCommand As String =
+                "CREATE TABLE IF NOT EXISTS
+                [" & TableName & "] (
+                [FileName] STRING ,"
+        For Each FITSKeyword As String In FITSKeywords
+            SQLCommand &= "[" & FITSKeyword & "] " & StdType & ","
+        Next FITSKeyword
+        SQLCommand = SQLCommand.Substring(0, SQLCommand.Length - 1) & ")"
+
+        Return SQLCommand
+
+    End Function
+
+End Class
