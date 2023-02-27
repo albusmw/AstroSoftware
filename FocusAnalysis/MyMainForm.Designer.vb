ebda76a2faec67e9c85a38c17ca60b0e6d4bfcf2
albusmw <albusmw@gmx.de>
2023-02-27 10:34:49 +0100

    FocusAnalysis added

---
+++ FocusAnalysis/MyMainForm.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MyMainForm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.msMain = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmiFile_OpenSerSequence = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ofdMain = New System.Windows.Forms.OpenFileDialog()
+        Me.gbStatus = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pbImageStream = New System.Windows.Forms.ProgressBar()
+        Me.tImageStream = New System.Windows.Forms.Label()
+        Me.msMain.SuspendLayout()
+        Me.gbStatus.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'msMain
+        '
+        Me.msMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.msMain.Location = New System.Drawing.Point(0, 0)
+        Me.msMain.Name = "msMain"
+        Me.msMain.Size = New System.Drawing.Size(725, 24)
+        Me.msMain.TabIndex = 0
+        Me.msMain.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmiFile_OpenSerSequence})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'tsmiFile_OpenSerSequence
+        '
+        Me.tsmiFile_OpenSerSequence.Name = "tsmiFile_OpenSerSequence"
+        Me.tsmiFile_OpenSerSequence.Size = New System.Drawing.Size(178, 22)
+        Me.tsmiFile_OpenSerSequence.Text = "Open SER sequence"
+        '
+        'ofdMain
+        '
+        Me.ofdMain.FileName = "OpenFileDialog1"
+        '
+        'gbStatus
+        '
+        Me.gbStatus.Controls.Add(Me.tImageStream)
+        Me.gbStatus.Controls.Add(Me.pbImageStream)
+        Me.gbStatus.Controls.Add(Me.Label1)
+        Me.gbStatus.Location = New System.Drawing.Point(12, 27)
+        Me.gbStatus.Name = "gbStatus"
+        Me.gbStatus.Size = New System.Drawing.Size(368, 157)
+        Me.gbStatus.TabIndex = 1
+        Me.gbStatus.TabStop = False
+        Me.gbStatus.Text = "Status"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Image stream"
+        '
+        'pbImageStream
+        '
+        Me.pbImageStream.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbImageStream.Location = New System.Drawing.Point(92, 29)
+        Me.pbImageStream.Name = "pbImageStream"
+        Me.pbImageStream.Size = New System.Drawing.Size(194, 13)
+        Me.pbImageStream.TabIndex = 2
+        '
+        'tImageStream
+        '
+        Me.tImageStream.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tImageStream.Location = New System.Drawing.Point(292, 29)
+        Me.tImageStream.Name = "tImageStream"
+        Me.tImageStream.Size = New System.Drawing.Size(70, 13)
+        Me.tImageStream.TabIndex = 3
+        Me.tImageStream.Text = "0/?"
+        Me.tImageStream.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'MyMainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 609)
+        Me.Controls.Add(Me.gbStatus)
+        Me.Controls.Add(Me.msMain)
+        Me.MainMenuStrip = Me.msMain
+        Me.Name = "MyMainForm"
+        Me.Text = "FocusAnalysis"
+        Me.msMain.ResumeLayout(False)
+        Me.msMain.PerformLayout()
+        Me.gbStatus.ResumeLayout(False)
+        Me.gbStatus.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents msMain As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tsmiFile_OpenSerSequence As ToolStripMenuItem
+    Friend WithEvents ofdMain As OpenFileDialog
+    Friend WithEvents gbStatus As GroupBox
+    Friend WithEvents tImageStream As Label
+    Friend WithEvents pbImageStream As ProgressBar
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FocusAnalysis/MyMainForm.vb
@@ -0,0 +1,113 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class MyMainForm
+
+    Private ReadOnly Property MyPath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly.Location)
+    Private IPPPath As String = String.Empty
+
+    '''<summary>Handle to Intel IPP functions.</summary>
+    Private IntelIPP As cIntelIPP
+
+    '''<summary>Statistics processor (for the last file).</summary>
+    Private SingleStatCalc As AstroNET.Statistics
+
+    '''<summary>Statistics of the last frame.</summary>
+    Private LastStat As AstroNET.Statistics.sStatistics
+
+    Private Sub tsmiFile_OpenSerSequence_Click(sender As Object, e As EventArgs) Handles tsmiFile_OpenSerSequence.Click
+
+        With ofdMain
+            .Filter = "SER file (*.ser)|*.ser"
+            If .ShowDialog <> DialogResult.OK Then Exit Sub
+        End With
+
+        'Open the SER file
+        Dim BinaryIN As New System.IO.BinaryReader(System.IO.File.OpenRead(ofdMain.FileName))
+        Dim SERHeader As New cSERFormat.cSERHeader(BinaryIN)
+        Dim TimeStamps As DateTime() = SERHeader.ReadTrailer(BinaryIN)
+
+        SingleStatCalc.ResetAllProcessors()
+
+        'Go on if stream position is ok
+        If BinaryIN.BaseStream.Position = cSERFormat.cSERHeader.SERHeaderLength Then
+
+            'We assume RGGB and 16 bit for now ...
+            If SERHeader.BytePerPixel = 2 Then
+
+                Dim FrameResults As New Dictionary(Of Integer, List(Of Object))
+
+                pbImageStream.Maximum = SERHeader.FrameCount
+                For FrameCountIdx As Integer = 1 To SERHeader.FrameCount
+
+                    Dim FrameResult As New List(Of Object)
+                    FrameResult.Add(FrameCountIdx)              'frame idx
+                    FrameResult.Add("???")                      'focus position
+
+                    pbImageStream.Value = FrameCountIdx
+                    tImageStream.Text = FrameCountIdx.ValRegIndep & "/" & pbImageStream.Maximum.ValRegIndep
+                    De()
+
+                    '1.) Read in 1 frame and convert to 2-byte data type
+                    Dim FullFrameSize As Integer = CInt(SERHeader.FrameWidth * SERHeader.FrameHeight * SERHeader.BytePerPixel)
+                    ReDim SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data(SERHeader.FrameWidth - 1, SERHeader.FrameHeight - 1)
+                    IntelIPP.Transpose(BinaryIN.ReadBytes(FullFrameSize), SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data)
+
+                    'Calculate generic statistics
+                    LastStat = SingleStatCalc.ImageStatistics
+
+                    'Process focus relevant data
+                    Dim EnergyPct As Double = 20.0
+                    Dim TotalEnery As Long = AstroNET.Statistics.TotalEnergy(LastStat.MonochromHistogram_Int)
+                    FrameResult.Add(TotalEnery)                 'total energy
+                    Dim BinFor80pct As UInt64 = AstroNET.Statistics.FocusQualityIndicator(LastStat.MonochromHistogram_Int, EnergyPct)
+                    FrameResult.Add(BinFor80pct)
+                    Dim BinFor80pct_R As UInt64 = AstroNET.Statistics.FocusQualityIndicator(LastStat.BayerHistograms_Int(0, 0), EnergyPct)
+                    FrameResult.Add(BinFor80pct_R)
+
+                    'Store results
+                    FrameResults.Add(FrameCountIdx, FrameResult) : De()
+
+                Next FrameCountIdx
+
+                'Indicate finished
+                pbImageStream.Value = 0
+                tImageStream.Text = pbImageStream.Maximum.ValRegIndep & " frames loaded"
+                De()
+
+                '4) Build EXCEL output, save and open
+                Using workbook As New ClosedXML.Excel.XLWorkbook
+                    Dim WorkSheet_Single As ClosedXML.Excel.IXLWorksheet = workbook.Worksheets.Add("Frame Statistics")
+                    WorkSheet_Single.Cell(1, 1).InsertData(New List(Of String)({"Frame #", "Focus position", "Total Energy", "Total 80pct pixel #", "R channel 80pct pixel #"}), True)
+                    For FrameCountIdx As Integer = 1 To SERHeader.FrameCount
+                        WorkSheet_Single.Cell(FrameCountIdx + 1, 1).InsertData(FrameResults(FrameCountIdx), True)
+                    Next FrameCountIdx
+                    For Each col In WorkSheet_Single.ColumnsUsed
+                        col.AdjustToContents()
+                    Next col
+                    Dim FileToGenerate As String = IO.Path.Combine(MyPath, "SERFocus.xlsx")
+                    workbook.SaveAs(FileToGenerate)
+                    Process.Start(FileToGenerate)
+                End Using
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub MyMainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Load IPP
+        Dim IPPLoadError As String = String.Empty
+        IPPPath = cIntelIPP.SearchDLLToUse(cIntelIPP.PossiblePaths(MyPath).ToArray, IPPLoadError)
+        If String.IsNullOrEmpty(IPPLoadError) = True Then
+            IntelIPP = New cIntelIPP(IPPPath)
+        End If
+        SingleStatCalc = New AstroNET.Statistics(IntelIPP)
+    End Sub
+
+    Private Sub De()
+        System.Windows.Forms.Application.DoEvents()
+    End Sub
+
+End Class
