1dde9c4d0285dff4cf4f8d7c06df5bc155bd970e
albusmw <albusmw@gmx.de>
2024-05-07 07:35:12 +0200

    InView: added code - now working correct

--- InView/Form1.Designer.vb
+++ InView/Form1.Designer.vb
@@ -22,8 +22,8 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         tbLog = New TextBox()
-        btnFromClipboard = New Button()
         msMain = New MenuStrip()
         tsmiFile = New ToolStripMenuItem()
         tsmiFile_Exit = New ToolStripMenuItem()
@@ -31,36 +31,42 @@ Partial Class MainForm
         tsmiCalc_Visibility = New ToolStripMenuItem()
         ToolStripMenuItem1 = New ToolStripSeparator()
         tsmiCalc_TC1 = New ToolStripMenuItem()
+        tsmiCalc_TC2 = New ToolStripMenuItem()
+        tsmiCalc_ClipAstrobin = New ToolStripMenuItem()
+        zgcMain = New ZedGraph.ZedGraphControl()
+        scMain = New SplitContainer()
+        Label2 = New Label()
+        tbObjectDec = New TextBox()
+        Label1 = New Label()
+        tbObjectRA = New TextBox()
         msMain.SuspendLayout()
+        CType(scMain, ComponentModel.ISupportInitialize).BeginInit()
+        scMain.Panel1.SuspendLayout()
+        scMain.Panel2.SuspendLayout()
+        scMain.SuspendLayout()
         SuspendLayout()
         ' 
         ' tbLog
         ' 
-        tbLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
-        tbLog.Font = New Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point)
-        tbLog.Location = New Point(12, 94)
+        tbLog.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tbLog.Font = New Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        tbLog.Location = New Point(0, 52)
+        tbLog.Margin = New Padding(3, 4, 3, 4)
         tbLog.Multiline = True
         tbLog.Name = "tbLog"
         tbLog.ScrollBars = ScrollBars.Both
-        tbLog.Size = New Size(860, 445)
+        tbLog.Size = New Size(531, 824)
         tbLog.TabIndex = 1
         tbLog.WordWrap = False
         ' 
-        ' btnFromClipboard
-        ' 
-        btnFromClipboard.Location = New Point(146, 56)
-        btnFromClipboard.Name = "btnFromClipboard"
-        btnFromClipboard.Size = New Size(164, 23)
-        btnFromClipboard.TabIndex = 2
-        btnFromClipboard.Text = "Astrobin Clipboard"
-        btnFromClipboard.UseVisualStyleBackColor = True
-        ' 
         ' msMain
         ' 
+        msMain.ImageScalingSize = New Size(20, 20)
         msMain.Items.AddRange(New ToolStripItem() {tsmiFile, tsmiCalc})
         msMain.Location = New Point(0, 0)
         msMain.Name = "msMain"
-        msMain.Size = New Size(884, 24)
+        msMain.Padding = New Padding(7, 3, 0, 3)
+        msMain.Size = New Size(1618, 30)
         msMain.TabIndex = 3
         msMain.Text = "MenuStrip1"
         ' 
@@ -68,57 +74,144 @@ Partial Class MainForm
         ' 
         tsmiFile.DropDownItems.AddRange(New ToolStripItem() {tsmiFile_Exit})
         tsmiFile.Name = "tsmiFile"
-        tsmiFile.Size = New Size(37, 20)
+        tsmiFile.Size = New Size(46, 24)
         tsmiFile.Text = "File"
         ' 
         ' tsmiFile_Exit
         ' 
         tsmiFile_Exit.Name = "tsmiFile_Exit"
-        tsmiFile_Exit.Size = New Size(93, 22)
+        tsmiFile_Exit.Size = New Size(116, 26)
         tsmiFile_Exit.Text = "Exit"
         ' 
         ' tsmiCalc
         ' 
-        tsmiCalc.DropDownItems.AddRange(New ToolStripItem() {tsmiCalc_Visibility, ToolStripMenuItem1, tsmiCalc_TC1})
+        tsmiCalc.DropDownItems.AddRange(New ToolStripItem() {tsmiCalc_Visibility, ToolStripMenuItem1, tsmiCalc_TC1, tsmiCalc_TC2, tsmiCalc_ClipAstrobin})
         tsmiCalc.Name = "tsmiCalc"
-        tsmiCalc.Size = New Size(68, 20)
+        tsmiCalc.Size = New Size(84, 24)
         tsmiCalc.Text = "Calculate"
         ' 
         ' tsmiCalc_Visibility
         ' 
         tsmiCalc_Visibility.Name = "tsmiCalc_Visibility"
-        tsmiCalc_Visibility.Size = New Size(212, 22)
+        tsmiCalc_Visibility.Size = New Size(268, 26)
         tsmiCalc_Visibility.Text = "Object visibility"
         ' 
         ' ToolStripMenuItem1
         ' 
         ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        ToolStripMenuItem1.Size = New Size(209, 6)
+        ToolStripMenuItem1.Size = New Size(265, 6)
         ' 
         ' tsmiCalc_TC1
         ' 
         tsmiCalc_TC1.Name = "tsmiCalc_TC1"
-        tsmiCalc_TC1.Size = New Size(212, 22)
+        tsmiCalc_TC1.Size = New Size(268, 26)
         tsmiCalc_TC1.Text = "Test case 1 (use debugger)"
+        ' 
+        ' tsmiCalc_TC2
+        ' 
+        tsmiCalc_TC2.Name = "tsmiCalc_TC2"
+        tsmiCalc_TC2.Size = New Size(268, 26)
+        tsmiCalc_TC2.Text = "Test case 2 (CCDGuide)"
+        ' 
+        ' tsmiCalc_ClipAstrobin
+        ' 
+        tsmiCalc_ClipAstrobin.Name = "tsmiCalc_ClipAstrobin"
+        tsmiCalc_ClipAstrobin.Size = New Size(268, 26)
+        tsmiCalc_ClipAstrobin.Text = "From clipboard (AstroBin)"
+        ' 
+        ' zgcMain
+        ' 
+        zgcMain.Dock = DockStyle.Fill
+        zgcMain.Location = New Point(0, 0)
+        zgcMain.Margin = New Padding(4, 5, 4, 5)
+        zgcMain.Name = "zgcMain"
+        zgcMain.ScrollGrace = 0R
+        zgcMain.ScrollMaxX = 0R
+        zgcMain.ScrollMaxY = 0R
+        zgcMain.ScrollMaxY2 = 0R
+        zgcMain.ScrollMinX = 0R
+        zgcMain.ScrollMinY = 0R
+        zgcMain.ScrollMinY2 = 0R
+        zgcMain.Size = New Size(1059, 876)
+        zgcMain.TabIndex = 4
+        ' 
+        ' scMain
+        ' 
+        scMain.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        scMain.Location = New Point(12, 42)
+        scMain.Name = "scMain"
+        ' 
+        ' scMain.Panel1
+        ' 
+        scMain.Panel1.Controls.Add(Label2)
+        scMain.Panel1.Controls.Add(tbObjectDec)
+        scMain.Panel1.Controls.Add(Label1)
+        scMain.Panel1.Controls.Add(tbObjectRA)
+        scMain.Panel1.Controls.Add(tbLog)
+        ' 
+        ' scMain.Panel2
+        ' 
+        scMain.Panel2.Controls.Add(zgcMain)
+        scMain.Size = New Size(1594, 876)
+        scMain.SplitterDistance = 531
+        scMain.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(243, 21)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(35, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Dec"
+        ' 
+        ' tbObjectDec
+        ' 
+        tbObjectDec.Location = New Point(284, 18)
+        tbObjectDec.Name = "tbObjectDec"
+        tbObjectDec.Size = New Size(233, 27)
+        tbObjectDec.TabIndex = 4
+        tbObjectDec.Text = "45"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 21)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(28, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "RA"
+        ' 
+        ' tbObjectRA
+        ' 
+        tbObjectRA.Location = New Point(46, 18)
+        tbObjectRA.Name = "tbObjectRA"
+        tbObjectRA.Size = New Size(191, 27)
+        tbObjectRA.TabIndex = 2
+        tbObjectRA.Text = "6h 15m 8s"
         ' 
         ' MainForm
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(884, 551)
-        Controls.Add(btnFromClipboard)
-        Controls.Add(tbLog)
+        ClientSize = New Size(1618, 930)
+        Controls.Add(scMain)
         Controls.Add(msMain)
         MainMenuStrip = msMain
+        Margin = New Padding(3, 4, 3, 4)
         Name = "MainForm"
         Text = "InView"
         msMain.ResumeLayout(False)
         msMain.PerformLayout()
+        scMain.Panel1.ResumeLayout(False)
+        scMain.Panel1.PerformLayout()
+        scMain.Panel2.ResumeLayout(False)
+        CType(scMain, ComponentModel.ISupportInitialize).EndInit()
+        scMain.ResumeLayout(False)
         ResumeLayout(False)
         PerformLayout()
     End Sub
     Friend WithEvents tbLog As TextBox
-    Friend WithEvents btnFromClipboard As Button
     Friend WithEvents msMain As MenuStrip
     Friend WithEvents tsmiFile As ToolStripMenuItem
     Friend WithEvents tsmiFile_Exit As ToolStripMenuItem
@@ -126,5 +219,13 @@ Partial Class MainForm
     Friend WithEvents tsmiCalc_Visibility As ToolStripMenuItem
     Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
     Friend WithEvents tsmiCalc_TC1 As ToolStripMenuItem
+    Friend WithEvents tsmiCalc_TC2 As ToolStripMenuItem
+    Friend WithEvents zgcMain As ZedGraph.ZedGraphControl
+    Friend WithEvents tsmiCalc_ClipAstrobin As ToolStripMenuItem
+    Friend WithEvents scMain As SplitContainer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tbObjectDec As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbObjectRA As TextBox
 
 End Class
--- InView/Form1.vb
+++ InView/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Option Explicit On
 Option Strict On
+Imports DocumentFormat.OpenXml.Office.Word
+Imports InView.AstroCalc.NET
 
 Public Class MainForm
 
@@ -9,11 +11,12 @@ Public Class MainForm
         Public Distance As Double
     End Structure
 
-
     Private DB As New cDB
+    Private Plotter As cZEDGraph
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        Plotter = New cZEDGraph(zgcMain)
         Dim X As New ASCOM.Tools.RiseSetTimes
 
         Calculate()
@@ -127,21 +130,6 @@ Public Class MainForm
 
     End Sub
 
-    Private Sub btnFromClipboard_Click(sender As Object, e As EventArgs) Handles btnFromClipboard.Click
-        'Parse the data from the coordinates given on the AstroBin object page
-        Dim ClipContent As String() = Split(Clipboard.GetText, System.Environment.NewLine)
-        Dim RA As Double = Double.NaN
-        Dim Dec As Double = Double.NaN
-        For Each Line As String In ClipContent
-            If Line.Trim.StartsWith("RA center:") Then
-                RA = AstroParser.ParseRA(Line.Trim.Replace("RA center:", String.Empty).Trim)
-            End If
-            If Line.Trim.StartsWith("DEC center:") Then
-                Dec = AstroParser.ParseDeclination(Line.Trim.Replace("DEC center:", String.Empty))
-            End If
-        Next Line
-    End Sub
-
     Private Sub tsmiFile_Exit_Click(sender As Object, e As EventArgs) Handles tsmiFile_Exit.Click
         End
     End Sub
@@ -169,7 +157,7 @@ Public Class MainForm
             If SelectedObject.HD > 0 Then Details.Add(" HD".PadLeft(LeftWidth) & ": " & SelectedObject.HD.ToString.Trim)
             Details.Add("   RA".PadLeft(LeftWidth) & ": " & Ato.AstroCalc.FormatHMS(SelectedObject.RA))
             Details.Add("   Dec".PadLeft(LeftWidth) & ": " & Ato.AstroCalc.Format360Degree(SelectedObject.Dec))
-            Details.Add("   Alt".PadLeft(LeftWidth) & ":  " & Ato.AstroCalc.Format360Degree(Pos.ALT))
+            Details.Add("   Alt".PadLeft(LeftWidth) & ":  " & Ato.AstroCalc.Format360Degree(Pos.Alt))
             Details.Add("   Az".PadLeft(LeftWidth) & ":  " & Ato.AstroCalc.Format360Degree(Pos.AZ))
             Details.Add("   Hour angle".PadLeft(LeftWidth) & ":  " & Ato.AstroCalc.FormatHMS(ObjectHourAngle * (24 / 360)))
             Details.Add("═══════════════════════════════════════════════════════")
@@ -190,6 +178,100 @@ Public Class MainForm
         tbLog.Text = Join(Ato.AstroCalc.RunTestCase_SelfBuild.ToArray, System.Environment.NewLine)
     End Sub
 
+    Private Sub tsmiCalc_TC2_Click(sender As Object, e As EventArgs) Handles tsmiCalc_TC2.Click
+
+        Dim AllResources As String() = Reflection.Assembly.GetExecutingAssembly.GetManifestResourceNames
+        Dim Content As String() = ReadAllLines(AllResources(1))
+
+        Dim CurrentLocation As New Ato.AstroCalc.sLatLong
+        Dim TimeZone As Integer = -4
+        Dim J2000 As New Ato.AstroCalc.sRADec
+
+        'Find start of tracking details
+        Dim TrackingDetailsStart As Integer = -1
+        For Idx As Integer = 0 To Content.GetUpperBound(0)
+            Dim Keyword As String = Content(Idx).Trim
+            If Keyword.ToUpper.StartsWith("Location".ToUpper) Then
+                Dim LongStr As String() = Content(Idx + 1).Trim.Split(" "c)
+                Dim LatStr As String() = Content(Idx + 2).Trim.Split(" "c)
+                CurrentLocation = New Ato.AstroCalc.sLatLong(cCCDGuide.GetDMS(LatStr, 15), cCCDGuide.GetDMS(LongStr, 14))
+            End If
+            If Keyword.ToUpper.StartsWith("J2000".ToUpper) Then
+                Dim Entry As String() = Content(Idx).Trim.Split(" "c)
+                J2000 = New Ato.AstroCalc.sRADec(cCCDGuide.GetHMS(Entry, 7), cCCDGuide.GetDMS(Entry, 12))
+            End If
+            If Keyword.ToUpper.StartsWith("Tracking details".ToUpper) Then
+                TrackingDetailsStart = Idx + 6
+            End If
+        Next Idx
+
+        'Plot some header data to the log
+        ClearLog()
+        AddToLog(" Location Lat  : " & Ato.AstroCalc.Format360Degree(CurrentLocation.Latitude_deg))
+        AddToLog(" Location Long : " & Ato.AstroCalc.Format360Degree(CurrentLocation.Longitude_deg))
+        AddToLog(" Object RA     : " & Ato.AstroCalc.FormatHMS(J2000.RA))
+        AddToLog(" Object Dec    : " & Ato.AstroCalc.Format360Degree(J2000.DEC))
+        AddToLog("Date      |Time      |DateTime           | AZ CCD   |ALT CCD   |  AZ      |ALT       | LST")
+
+        Dim Alt_CCD As New List(Of Double)
+        Dim Azi_CCD As New List(Of Double)
+        Dim Alt_Own As New List(Of Double)
+        Dim Azi_Own As New List(Of Double)
+
+        'Get data and compare with own generated data
+        For Idx As Integer = TrackingDetailsStart To Content.GetUpperBound(0)
+            'Get input data from CCDGuide table
+            Dim SingleLine As String() = cCCDGuide.OnlySingleSpaces(Content(Idx)).Split(" ")
+            Dim MomentLocal As DateTime = cCCDGuide.GetDateTime(SingleLine, 0)      'CCDGuide time and date are local
+            Dim MomentUTC As DateTime = MomentLocal.AddHours(-TimeZone)
+            'Calculate
+            Dim ObjectHourAngle As Double = Double.NaN
+            Dim JulianDay As Double = Ato.AstroCalc.JulianDateTime(MomentUTC)
+            Dim LocalStarTime As String = Ato.AstroCalc.LSTFormated(MomentUTC, CurrentLocation.Longitude_deg)
+            Dim CCDAltAz As New Ato.AstroCalc.sAzAlt(SingleLine(2).ValRegIndep, SingleLine(3).ValRegIndep)
+            Dim MyAltAz As Ato.AstroCalc.sAzAlt = Ato.AstroCalc.GetObjectPosition(MomentUTC, CurrentLocation, J2000, ObjectHourAngle)
+            'Store data for display
+            Alt_CCD.Add(CCDAltAz.Alt)
+            Azi_CCD.Add(CCDAltAz.AZ)
+            Alt_Own.Add(MyAltAz.Alt)
+            Azi_Own.Add(MyAltAz.AZ)
+            'Output data to the log
+            Dim LogLine As New List(Of String)
+            LogLine.Add(SingleLine(0).PadLeft(10, " "c))                            'Date
+            LogLine.Add(SingleLine(1).PadLeft(10, " "c))                            'Time
+            LogLine.Add(MomentLocal.ValRegIndep)                                    'Moment [parsed]
+            LogLine.Add(SingleLine(2).PadLeft(10, " "c))                            'Azimuth [CCDGuide]
+            LogLine.Add(SingleLine(3).PadLeft(10, " "c))                            'Altitude [CCDGuide]
+            LogLine.Add(MyAltAz.AZ.ValRegIndep("000.000").PadLeft(10, " "c))        'Azimuth [self calculated]
+            LogLine.Add(MyAltAz.Alt.ValRegIndep("##0.000").PadLeft(10, " "c))       'Altitude [self calculated]
+            LogLine.Add(LocalStarTime.PadLeft(10, " "c))                            'LocalStartTime [self calculated]
+            AddToLog(Join(LogLine.ToArray, "|"))
+        Next Idx
+
+        'Current status: - Altitude is correct (only small calculation errors).
+        '                - Azimuth seems to be as "half speed" or "wrong sign"  
+
+        'Plotter.PlotXvsY("CCDGuide", Azi_CCD.ToArray, Alt_CCD.ToArray, New cZEDGraph.sGraphStyle(Color.Green, cZEDGraph.eCurveMode.Dots))
+        'Plotter.PlotXvsY("My own", Azi_Own.ToArray, Alt_Own.ToArray, New cZEDGraph.sGraphStyle(Color.Red, cZEDGraph.eCurveMode.Dots))
+        Plotter.SetCaptions("Object trace", "Azimuth", "Altitude")
+        Plotter.PlotData("CCDGuide", Azi_CCD.ToArray, New cZEDGraph.sGraphStyle(Color.Green, cZEDGraph.eCurveMode.Lines))
+        Plotter.PlotData("My own", Azi_Own.ToArray, New cZEDGraph.sGraphStyle(Color.Red, cZEDGraph.eCurveMode.Dots))
+        Plotter.SetCaptions("Object trace", "Calculation index", "Azimuth")
+        Plotter.ManuallyScaleYAxisLin(0, 360)
+        Plotter.ForceUpdate()
+
+    End Sub
+
+    Private Function ReadAllLines(ByRef ResourceName As String) As String()
+        Dim RetVal As New List(Of String)
+        Dim ResourceStream As System.IO.Stream = Reflection.Assembly.GetExecutingAssembly.GetManifestResourceStream(ResourceName)
+        Dim TextStream As New System.IO.StreamReader(ResourceStream, System.Text.Encoding.UTF8)
+        Do While TextStream.EndOfStream = False
+            RetVal.Add(TextStream.ReadLine())
+        Loop
+        Return RetVal.ToArray
+    End Function
+
     '══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
     ' Log output
     '══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
@@ -204,6 +286,94 @@ Public Class MainForm
         Else
             tbLog.Text &= Text
         End If
+    End Sub
+
+    Private Sub tsmiCalc_ClipAstrobin_Click(sender As Object, e As EventArgs) Handles tsmiCalc_ClipAstrobin.Click
+        'Parse the data from the coordinates given on the AstroBin object page
+        Dim ClipContent As String() = Split(Clipboard.GetText, System.Environment.NewLine)
+        Dim RA As Double = Double.NaN
+        Dim Dec As Double = Double.NaN
+        For Each Line As String In ClipContent
+            If Line.Trim.StartsWith("RA center:") Then
+                RA = AstroParser.ParseRA(Line.Trim.Replace("RA center:", String.Empty).Trim)
+            End If
+            If Line.Trim.StartsWith("DEC center:") Then
+                Dec = AstroParser.ParseDeclination(Line.Trim.Replace("DEC center:", String.Empty))
+            End If
+        Next Line
+    End Sub
+
+    Private Sub EntryUpdate(sender As Object, e As EventArgs) Handles tbObjectRA.TextChanged, tbObjectDec.TextChanged
+        CalcAll()
+    End Sub
+
+    Private Sub CalcAll()
+
+        Dim UTC_Start As DateTime = Now.ToUniversalTime
+        Dim UTC_Stop As DateTime = UTC_Start.AddDays(7)
+        Dim UTC_Delta As New TimeSpan(0, 1, 0)
+
+        'Form timeline
+        Dim MomentUTC As DateTime = UTC_Start
+        Dim TimeLine As New List(Of DateTime)
+        Do
+            TimeLine.Add(MomentUTC)
+            MomentUTC = MomentUTC.Add(UTC_Delta)
+            If MomentUTC >= UTC_Stop Then Exit Do
+        Loop Until 1 = 0
+
+        'Calculate all positions
+        Dim CurrentLocation As Ato.AstroCalc.sLatLong = Ato.AstroCalc.KnownLocations.DSC
+        Dim J2000 As New Ato.AstroCalc.sRADec(tbObjectRA.Text, tbObjectDec.Text)
+        Dim Stopper As New Stopwatch : Stopper.Reset() :
+        Stopper.Start()
+        Dim Positions As Ato.AstroCalc.sAzAlt() = Ato.AstroCalc.GetObjectPosition(TimeLine.ToArray, CurrentLocation, J2000, Nothing)
+        Stopper.Stop()
+        ClearLog()
+        AddToLog("Calculation took " & Stopper.ElapsedMilliseconds & " ms")
+
+        'Plot
+        Dim SunHeights As New List(Of Double)
+        Dim Object_Azimuth As New List(Of Double)
+        Dim Object_Altitude As New List(Of Double)
+        Dim Object_AltitudeOK As New List(Of Double)
+        For Idx As Integer = 0 To Positions.GetUpperBound(0)
+            Dim SunAz As Double = Double.NaN
+            Dim SunHeight As Double = Double.NaN
+            Sun.SunPos(TimeLine(Idx), CurrentLocation.Longitude_deg, CurrentLocation.Latitude_deg, SunAz, SunHeight)
+            SunHeights.Add(SunHeight)
+            Object_Azimuth.Add(Positions(Idx).AZ_deg)
+            Object_Altitude.Add(Positions(Idx).ALT_deg)
+            'Observable
+            If (SunHeight <= -6) And (Positions(Idx).ALT_deg >= 10) Then
+                Object_AltitudeOK.Add(Positions(Idx).ALT_deg)
+            Else
+                Object_AltitudeOK.Add(Double.NaN)
+            End If
+        Next Idx
+        If IsNothing(Plotter) = True Then Exit Sub
+
+        'Plot
+        Plotter.Clear()
+        Plotter.SetCaptions("Object trace", "Time", "Altitude")
+        Plotter.PlotXvsT("Trace", TimeLine.ToArray, Object_AltitudeOK.ToArray, New cZEDGraph.sGraphStyle(Color.Green, cZEDGraph.eCurveMode.Dots), False)
+        zgcMain.GraphPane.XAxis.Type = ZedGraph.AxisType.Date
+        'Plotter.ManuallyScaleXAxisLin(TimeLine.First.ToOADate, TimeLine.Last.ToOADate)
+        Plotter.ManuallyScaleYAxisLin(0, 90)
+        Plotter.ForceUpdate()
+
+    End Sub
+
+    Private Sub tbObjectDec_MouseWheel(sender As Object, e As MouseEventArgs) Handles tbObjectDec.MouseWheel
+        Dim CurrentValue As Double = AstroParser.ParseDeclination(tbObjectDec.Text)
+        CurrentValue += (10 / 60) * Math.Sign(e.Delta)
+        tbObjectDec.Text = Ato.AstroCalc.Format360Degree(CurrentValue)
+    End Sub
+
+    Private Sub tbObjectRA_MouseWheel(sender As Object, e As MouseEventArgs) Handles tbObjectRA.MouseWheel
+        Dim CurrentValue As Double = AstroParser.ParseRA(tbObjectRA.Text)
+        CurrentValue += (10 / (24 * 60)) * Math.Sign(e.Delta)
+        tbObjectRA.Text = Ato.AstroCalc.FormatHMS(CurrentValue)
     End Sub
 
 End Class
