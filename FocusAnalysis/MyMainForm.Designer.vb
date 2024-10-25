075590a4b9f5b9887c420fada21294ed93c14cee
Martin Weiss <albusmw@gmx.de>
2024-10-25 15:41:59 +0200

    FocusAnalysis extended

--- FocusAnalysis/MyMainForm.Designer.vb
+++ FocusAnalysis/MyMainForm.Designer.vb
@@ -22,100 +22,177 @@ Partial Class MyMainForm
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.msMain = New System.Windows.Forms.MenuStrip()
-        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.tsmiFile_OpenSerSequence = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ofdMain = New System.Windows.Forms.OpenFileDialog()
-        Me.gbStatus = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.pbImageStream = New System.Windows.Forms.ProgressBar()
-        Me.tImageStream = New System.Windows.Forms.Label()
-        Me.msMain.SuspendLayout()
-        Me.gbStatus.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'msMain
-        '
-        Me.msMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
-        Me.msMain.Location = New System.Drawing.Point(0, 0)
-        Me.msMain.Name = "msMain"
-        Me.msMain.Size = New System.Drawing.Size(725, 24)
-        Me.msMain.TabIndex = 0
-        Me.msMain.Text = "MenuStrip1"
-        '
-        'FileToolStripMenuItem
-        '
-        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmiFile_OpenSerSequence})
-        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
-        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
-        Me.FileToolStripMenuItem.Text = "File"
-        '
-        'tsmiFile_OpenSerSequence
-        '
-        Me.tsmiFile_OpenSerSequence.Name = "tsmiFile_OpenSerSequence"
-        Me.tsmiFile_OpenSerSequence.Size = New System.Drawing.Size(178, 22)
-        Me.tsmiFile_OpenSerSequence.Text = "Open SER sequence"
-        '
-        'ofdMain
-        '
-        Me.ofdMain.FileName = "OpenFileDialog1"
-        '
-        'gbStatus
-        '
-        Me.gbStatus.Controls.Add(Me.tImageStream)
-        Me.gbStatus.Controls.Add(Me.pbImageStream)
-        Me.gbStatus.Controls.Add(Me.Label1)
-        Me.gbStatus.Location = New System.Drawing.Point(12, 27)
-        Me.gbStatus.Name = "gbStatus"
-        Me.gbStatus.Size = New System.Drawing.Size(368, 157)
-        Me.gbStatus.TabIndex = 1
-        Me.gbStatus.TabStop = False
-        Me.gbStatus.Text = "Status"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(16, 29)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(70, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Image stream"
-        '
-        'pbImageStream
-        '
-        Me.pbImageStream.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.pbImageStream.Location = New System.Drawing.Point(92, 29)
-        Me.pbImageStream.Name = "pbImageStream"
-        Me.pbImageStream.Size = New System.Drawing.Size(194, 13)
-        Me.pbImageStream.TabIndex = 2
-        '
-        'tImageStream
-        '
-        Me.tImageStream.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.tImageStream.Location = New System.Drawing.Point(292, 29)
-        Me.tImageStream.Name = "tImageStream"
-        Me.tImageStream.Size = New System.Drawing.Size(70, 13)
-        Me.tImageStream.TabIndex = 3
-        Me.tImageStream.Text = "0/?"
-        Me.tImageStream.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'MyMainForm
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(725, 609)
-        Me.Controls.Add(Me.gbStatus)
-        Me.Controls.Add(Me.msMain)
-        Me.MainMenuStrip = Me.msMain
-        Me.Name = "MyMainForm"
-        Me.Text = "FocusAnalysis"
-        Me.msMain.ResumeLayout(False)
-        Me.msMain.PerformLayout()
-        Me.gbStatus.ResumeLayout(False)
-        Me.gbStatus.PerformLayout()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+        msMain = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        tsmiFile_OpenSerSequence = New ToolStripMenuItem()
+        ofdMain = New OpenFileDialog()
+        gbStatus = New GroupBox()
+        tbLog = New TextBox()
+        tImageStream = New Label()
+        pbImageStream = New ProgressBar()
+        Label1 = New Label()
+        tbSERFile = New TextBox()
+        Label2 = New Label()
+        tbPWI4LogFile = New TextBox()
+        Label3 = New Label()
+        btnAnalysis = New Button()
+        msMain.SuspendLayout()
+        gbStatus.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' msMain
+        ' 
+        msMain.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        msMain.Location = New Point(0, 0)
+        msMain.Name = "msMain"
+        msMain.Padding = New Padding(7, 2, 0, 2)
+        msMain.Size = New Size(760, 24)
+        msMain.TabIndex = 0
+        msMain.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {tsmiFile_OpenSerSequence})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' tsmiFile_OpenSerSequence
+        ' 
+        tsmiFile_OpenSerSequence.Name = "tsmiFile_OpenSerSequence"
+        tsmiFile_OpenSerSequence.Size = New Size(178, 22)
+        tsmiFile_OpenSerSequence.Text = "Open SER sequence"
+        ' 
+        ' ofdMain
+        ' 
+        ofdMain.FileName = "OpenFileDialog1"
+        ' 
+        ' gbStatus
+        ' 
+        gbStatus.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        gbStatus.Controls.Add(tbLog)
+        gbStatus.Controls.Add(tImageStream)
+        gbStatus.Controls.Add(pbImageStream)
+        gbStatus.Controls.Add(Label1)
+        gbStatus.Location = New Point(13, 120)
+        gbStatus.Margin = New Padding(4, 3, 4, 3)
+        gbStatus.Name = "gbStatus"
+        gbStatus.Padding = New Padding(4, 3, 4, 3)
+        gbStatus.Size = New Size(734, 396)
+        gbStatus.TabIndex = 1
+        gbStatus.TabStop = False
+        gbStatus.Text = "Status"
+        ' 
+        ' tbLog
+        ' 
+        tbLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tbLog.Font = New Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        tbLog.Location = New Point(7, 54)
+        tbLog.Multiline = True
+        tbLog.Name = "tbLog"
+        tbLog.ReadOnly = True
+        tbLog.ScrollBars = ScrollBars.Both
+        tbLog.Size = New Size(720, 336)
+        tbLog.TabIndex = 4
+        tbLog.WordWrap = False
+        ' 
+        ' tImageStream
+        ' 
+        tImageStream.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        tImageStream.Location = New Point(646, 33)
+        tImageStream.Margin = New Padding(4, 0, 4, 0)
+        tImageStream.Name = "tImageStream"
+        tImageStream.Size = New Size(82, 15)
+        tImageStream.TabIndex = 3
+        tImageStream.Text = "0/?"
+        tImageStream.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' pbImageStream
+        ' 
+        pbImageStream.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        pbImageStream.Location = New Point(107, 33)
+        pbImageStream.Margin = New Padding(4, 3, 4, 3)
+        pbImageStream.Name = "pbImageStream"
+        pbImageStream.Size = New Size(531, 15)
+        pbImageStream.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(19, 33)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(79, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Image stream"
+        ' 
+        ' tbSERFile
+        ' 
+        tbSERFile.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        tbSERFile.Location = New Point(91, 27)
+        tbSERFile.Name = "tbSERFile"
+        tbSERFile.Size = New Size(657, 23)
+        tbSERFile.TabIndex = 2
+        tbSERFile.Text = "\\192.168.100.10\dsc\2024_10_23 - Focus Videos\Focus Ha\04_42_51.ser"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 30)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(45, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "SER file"
+        ' 
+        ' tbPWI4LogFile
+        ' 
+        tbPWI4LogFile.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        tbPWI4LogFile.Location = New Point(91, 56)
+        tbPWI4LogFile.Name = "tbPWI4LogFile"
+        tbPWI4LogFile.Size = New Size(657, 23)
+        tbPWI4LogFile.TabIndex = 4
+        tbPWI4LogFile.Text = "\\192.168.100.10\dsc\2024_10_23 - Focus Videos\Focus Ha\04_42_51.log"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 59)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(73, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "PWI4 log file"
+        ' 
+        ' btnAnalysis
+        ' 
+        btnAnalysis.Location = New Point(14, 85)
+        btnAnalysis.Name = "btnAnalysis"
+        btnAnalysis.Size = New Size(657, 29)
+        btnAnalysis.TabIndex = 6
+        btnAnalysis.Text = "Analysis"
+        btnAnalysis.UseVisualStyleBackColor = True
+        ' 
+        ' MyMainForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(760, 528)
+        Controls.Add(btnAnalysis)
+        Controls.Add(Label3)
+        Controls.Add(tbPWI4LogFile)
+        Controls.Add(Label2)
+        Controls.Add(tbSERFile)
+        Controls.Add(gbStatus)
+        Controls.Add(msMain)
+        MainMenuStrip = msMain
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "MyMainForm"
+        Text = "FocusAnalysis"
+        msMain.ResumeLayout(False)
+        msMain.PerformLayout()
+        gbStatus.ResumeLayout(False)
+        gbStatus.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
 
     End Sub
 
@@ -127,4 +204,10 @@ Partial Class MyMainForm
     Friend WithEvents tImageStream As Label
     Friend WithEvents pbImageStream As ProgressBar
     Friend WithEvents Label1 As Label
+    Friend WithEvents tbSERFile As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tbPWI4LogFile As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnAnalysis As Button
+    Friend WithEvents tbLog As TextBox
 End Class
--- FocusAnalysis/MyMainForm.vb
+++ FocusAnalysis/MyMainForm.vb
@@ -1,13 +1,11 @@
 ﻿Option Explicit On
 Option Strict On
-Imports FocusAnalysis.cSERFormat
-
-
 
 #Disable Warning CA1416 ' Validate platform compatibility
 Public Class MyMainForm
 
     Private ReadOnly Property MyPath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly.Location)
+
     Private IPPPath As String = String.Empty
 
     '''<summary>Handle to Intel IPP functions.</summary>
@@ -17,19 +15,53 @@ Public Class MyMainForm
     Private SingleStatCalc As AstroNET.Statistics
 
     '''<summary>Statistics of the last frame.</summary>
-    Private LastStat As AstroNET.Statistics.sStatistics
+    'Private LastStat As AstroNET.Statistics.sStatistics
+
+    Private DD1 As Ato.DragDrop
+    Private DD2 As Ato.DragDrop
 
     Private Sub tsmiFile_OpenSerSequence_Click(sender As Object, e As EventArgs) Handles tsmiFile_OpenSerSequence.Click
 
         With ofdMain
             .Filter = "SER file (*.ser)|*.ser"
             If .ShowDialog <> DialogResult.OK Then Exit Sub
+            tbSERFile.Text = .FileName
         End With
 
+    End Sub
+
+    Private Sub MyMainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Load IPP
+        Dim IPPLoadError As String = String.Empty
+        IPPPath = cIntelIPP.SearchDLLToUse(cIntelIPP.PossiblePaths(MyPath).ToArray, IPPLoadError)
+        If String.IsNullOrEmpty(IPPLoadError) = True Then
+            IntelIPP = New cIntelIPP(IPPPath)
+        End If
+        SingleStatCalc = New AstroNET.Statistics(IntelIPP)
+        'Drag-and-drop
+        DD1 = New Ato.DragDrop(tbSERFile, True)
+        DD2 = New Ato.DragDrop(tbPWI4LogFile, True)
+    End Sub
+
+    Private Sub btnAnalysis_Click(sender As Object, e As EventArgs) Handles btnAnalysis.Click
+        Analysis()
+    End Sub
+
+    Private Sub Analysis()
+
+        tbLog.Text = String.Empty
+
+        'Load PWI4 file
+        Dim PWI4Log As List(Of KeyValuePair(Of DateTime, Double)) = LoadPWI4Log(tbPWI4LogFile.Text)
+        tbLog.Text &= "PWI4Log first moment: " & PWI4Log.First.Key.LongWithMS & System.Environment.NewLine
+        tbLog.Text &= "PWI4Log last moment : " & PWI4Log.Last.Key.LongWithMS & System.Environment.NewLine
+
         'Open the SER file
-        Dim BinaryIN As New System.IO.BinaryReader(System.IO.File.OpenRead(ofdMain.FileName))
+        Dim BinaryIN As New System.IO.BinaryReader(System.IO.File.OpenRead(tbSERFile.Text))
         Dim SERHeader As New cSERFormat.cSERHeader(BinaryIN)
         Dim TimeStamps As DateTime() = SERHeader.ReadTrailer(BinaryIN)
+        tbLog.Text &= "SER first moment    : " & TimeStamps.First.LongWithMS & System.Environment.NewLine
+        tbLog.Text &= "SER last moment     : " & TimeStamps.Last.LongWithMS & System.Environment.NewLine
 
         SingleStatCalc.ResetAllProcessors()
 
@@ -39,6 +71,7 @@ Public Class MyMainForm
         Dim FullFrameSize As Integer = CInt(SERHeader.FrameWidth * SERHeader.FrameHeight * SERHeader.BytePerPixel)
         ReDim SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data(SERHeader.FrameWidth - 1, SERHeader.FrameHeight - 1)
 
+        '──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
         '1st run: global maximum and minimum
         Dim GlobalMax(SERHeader.FrameWidth - 1, SERHeader.FrameHeight - 1) As UInt16 : GlobalMax.Init(UInt16.MinValue)
         Dim GlobalMin(SERHeader.FrameWidth - 1, SERHeader.FrameHeight - 1) As UInt16 : GlobalMin.Init(UInt16.MaxValue)
@@ -52,60 +85,94 @@ Public Class MyMainForm
             De()
         Next FrameCountIdx
 
-        'Reposition again
+        '──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+        'Reposition SER pointer again
         BinaryIN.BaseStream.Position = cSERFormat.cSERHeader.SERHeaderLength
 
+        'This are all results which will be available in the EXCEL table
         Dim FrameResults As New Dictionary(Of Integer, List(Of Object))
         Dim ResultParameters As New List(Of String)
         With ResultParameters
             .Add("Frame #")
             .Add("Time stamp")
+            .Add("Focus moment")
             .Add("Focus position")
             .Add("Sum of all pixel - raw")
             .Add("Sum of all pixel - corrected")
             .Add("Pixel for 10pct energy")
             .Add("Pixel for 20pct energy")
-            .Add("Pixel for 50pct energy")
-            .Add("Saturated #")
-            .Add("Saturated value")
+            .Add("Number of saturated pixel")
+            .Add("Saturation ADU value")
         End With
 
+        Dim FocusFit_X As New List(Of Double)
+        Dim FocusFit_Y As New List(Of Double)
+
+        'Move over all frames
         For FrameCountIdx As Integer = 0 To SERHeader.FrameCount - 1
 
-            Dim FrameResult As New Dictionary(Of String, Object)
-            FrameResult.Add("Frame #", FrameCountIdx)                                   'frame idx
-            FrameResult.Add("Time stamp", TimeStamps(FrameCountIdx).LongWithMS)         'frame idx
-            FrameResult.Add("Focus position", "???")                                    'focus position
-
+            'Show status start
             pbImageStream.Value = FrameCountIdx
             tImageStream.Text = FrameCountIdx.ValRegIndep & "/" & pbImageStream.Maximum.ValRegIndep
             De()
 
+            'Get timestamp and focus informations stored
+            Dim FrameResult As New Dictionary(Of String, Object)
+            Dim BestFocus As KeyValuePair(Of DateTime, Double) = FocusInMoment(PWI4Log, TimeStamps(FrameCountIdx))
+
+            FrameResult.Add("Frame #", FrameCountIdx)                                               'frame idx
+            FrameResult.Add("Time stamp", TimeStamps(FrameCountIdx).LongWithMS)                     'frame time stamp
+            FrameResult.Add("Focus moment", BestFocus.Key.LongWithMS)                               'focus time stamp
+            FrameResult.Add("Focus position", BestFocus.Value)                                      'focus position
+
             '1.) Read in 1 frame and convert to 2-byte data type - TODO: Little / Big Endian
             IntelIPP.Transpose(BinaryIN.ReadBytes(FullFrameSize), SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data)
-            LastStat = SingleStatCalc.ImageStatistics
-            FrameResult.Add("Saturated #", LastStat.MonochromHistogram_Int.Last.Value)
-            FrameResult.Add("Saturated value", LastStat.MonochromHistogram_Int.Last.Key)
-            FrameResult.Add("Sum of all pixel - raw", AstroNET.Statistics.TotalEnergy(LastStat.MonochromHistogram_Int))
 
-            '2.) Correct by min-of-all frame (black current, dead cold or hot pixel)
+            '2.) Calculate original statistics and report this
+            Dim OriginalStat As AstroNET.Statistics.sStatistics = SingleStatCalc.ImageStatistics
+            FrameResult.Add("Number of saturated pixel", OriginalStat.MonochromHistogram_Int.Last.Value)
+            FrameResult.Add("Saturation ADU value", OriginalStat.MonochromHistogram_Int.Last.Key)
+            FrameResult.Add("Sum of all pixel - raw", AstroNET.Statistics.TotalEnergy(OriginalStat.MonochromHistogram_Int))
+
+            '3.) Change the frame as required - subtract the "master dark" (per-pixel minimum value)
+            Dim ChangedStat As AstroNET.Statistics.sStatistics
             SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data = SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data.Subtract(GlobalMin)
-            LastStat = SingleStatCalc.ImageStatistics
+            ChangedStat = SingleStatCalc.ImageStatistics
 
-            '3.) Process focus relevant data
-            FrameResult.Add("Sum of all pixel - corrected", AstroNET.Statistics.TotalEnergy(LastStat.MonochromHistogram_Int))
-            FrameResult.Add("Pixel for 10pct energy", AstroNET.Statistics.FocusQualityIndicator(LastStat.MonochromHistogram_Int, 10.0))
-            FrameResult.Add("Pixel for 20pct energy", AstroNET.Statistics.FocusQualityIndicator(LastStat.MonochromHistogram_Int, 20.0))
-            FrameResult.Add("Pixel for 50pct energy", AstroNET.Statistics.FocusQualityIndicator(LastStat.MonochromHistogram_Int, 50.0))
+            '3.) Change the frame as required - set all pixel below x-percentil to zero
+            Dim ClipLimit As UInt16 = CUShort(ChangedStat.MonochromHistogram_PctFract(90))
+            SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data = SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data.ClipBelow(ClipLimit, UInt16.MinValue)
+            ChangedStat = SingleStatCalc.ImageStatistics
 
-            '4.) Store results
+            '4.) Process focus relevant data
+            FrameResult.Add("Sum of all pixel - corrected", AstroNET.Statistics.TotalEnergy(ChangedStat.MonochromHistogram_Int))
+            FrameResult.Add("Pixel for 10pct energy", AstroNET.Statistics.FocusQualityIndicator(ChangedStat.MonochromHistogram_Int, 10.0))
+            FrameResult.Add("Pixel for 20pct energy", AstroNET.Statistics.FocusQualityIndicator(ChangedStat.MonochromHistogram_Int, 20.0))
+
+            '5.) Store results
             Dim ExcelRow As New List(Of Object)
             For Each Parameter As String In ResultParameters
                 ExcelRow.Add(FrameResult(Parameter))
             Next Parameter
-            FrameResults.Add(FrameCountIdx, ExcelRow) : De()
+            FrameResults.Add(FrameCountIdx, ExcelRow)
+
+            If Double.IsNaN(BestFocus.Value) = False Then
+                FocusFit_X.Add(BestFocus.Value)
+                FocusFit_Y.Add(AstroNET.Statistics.FocusQualityIndicator(ChangedStat.MonochromHistogram_Int, 10.0))
+            End If
 
         Next FrameCountIdx
+
+        'Fit polynomial
+        Dim FitPolynom As Double() = {}
+        SignalProcessing.RegressPoly(FocusFit_X.ToArray, FocusFit_Y.ToArray, 2, FitPolynom)
+        tbLog.Text &= "FitPolynom - A0: " & FitPolynom(0).ValRegIndep & System.Environment.NewLine      'c
+        tbLog.Text &= "FitPolynom - A1: " & FitPolynom(1).ValRegIndep & System.Environment.NewLine      'b*x
+        tbLog.Text &= "FitPolynom - A2: " & FitPolynom(2).ValRegIndep & System.Environment.NewLine      'a*x²
+
+        'Calculate zero point of deviation
+        Dim OptFocus As Double = (-FitPolynom(1)) / (2 * FitPolynom(2))
+        tbLog.Text &= "OptFocus: " & OptFocus.ValRegIndep & System.Environment.NewLine
 
         'Indicate finished
         pbImageStream.Value = 0
@@ -127,18 +194,45 @@ Public Class MyMainForm
             Ato.Utils.StartWithItsEXE(FileToGenerate)
         End Using
 
-
     End Sub
 
-    Private Sub MyMainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'Load IPP
-        Dim IPPLoadError As String = String.Empty
-        IPPPath = cIntelIPP.SearchDLLToUse(cIntelIPP.PossiblePaths(MyPath).ToArray, IPPLoadError)
-        If String.IsNullOrEmpty(IPPLoadError) = True Then
-            IntelIPP = New cIntelIPP(IPPPath)
-        End If
-        SingleStatCalc = New AstroNET.Statistics(IntelIPP)
-    End Sub
+    '''<summary>Read the PWI4DataRecorder log file to get the focus positions.</summary>
+    '''<param name="FileName">PWI4 log file.</param>
+    '''<returns>All found timing stamps and focus positions.</returns>
+    Private Function LoadPWI4Log(ByVal FileName As String) As List(Of KeyValuePair(Of DateTime, Double))
+        If System.IO.File.Exists(FileName) = False Then Return Nothing
+        Dim AllLines As String() = System.IO.File.ReadAllLines(FileName)
+        If AllLines.Count < 2 Then Return Nothing
+        Dim RetVal As New List(Of KeyValuePair(Of DateTime, Double))
+        For Idx As Integer = 1 To AllLines.GetUpperBound(0)
+            Dim Splitted As String() = Split(AllLines(Idx), "|")
+            Dim Moment As DateTime = Splitted(2).Parse_LongWithMS
+            Dim FocusPos As Double = Splitted.Last.ValRegIndep
+            RetVal.Add(New KeyValuePair(Of Date, Double)(Moment, FocusPos))
+        Next Idx
+        Return RetVal
+    End Function
+
+    '''<summary>Get focus of the requested moment.</summary>
+    Private Function FocusInMoment(ByRef MomentAndPosition As List(Of KeyValuePair(Of DateTime, Double)), ByVal RequestedDateTime As DateTime) As KeyValuePair(Of DateTime, Double)
+        Dim BestValueLeft As New KeyValuePair(Of DateTime, Double)(DateTime.MinValue, Double.NaN)
+        Dim BestValueRight As New KeyValuePair(Of DateTime, Double)(DateTime.MaxValue, Double.NaN)
+        For Each Moment As KeyValuePair(Of DateTime, Double) In MomentAndPosition
+            Dim MomentDelta As Double = (Moment.Key - RequestedDateTime).TotalMilliseconds
+            If (MomentDelta < 0) Then
+                Dim BestDeltaLeft As Double = (BestValueLeft.Key - RequestedDateTime).TotalMilliseconds
+                If Math.Abs(MomentDelta) < Math.Abs(BestDeltaLeft) Then BestValueLeft = Moment
+            End If
+            If (MomentDelta > 0) Then
+                Dim BestDeltaRight As Double = (BestValueRight.Key - RequestedDateTime).TotalMilliseconds
+                If (MomentDelta > 0) And (Math.Abs(MomentDelta) < Math.Abs(BestDeltaRight)) Then BestValueRight = Moment
+            End If
+        Next Moment
+        Dim DeltaLeft As Double = (BestValueLeft.Key - RequestedDateTime).TotalMilliseconds
+        Dim DeltaRight As Double = (BestValueRight.Key - RequestedDateTime).TotalMilliseconds
+        Dim FocusPos As Double = MathEx.LinInterpolation(DeltaLeft, BestValueLeft.Value, DeltaRight, BestValueRight.Value, 0)
+        Return New KeyValuePair(Of DateTime, Double)(RequestedDateTime, FocusPos)
+    End Function
 
     Private Sub De()
         System.Windows.Forms.Application.DoEvents()
