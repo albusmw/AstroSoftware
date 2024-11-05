6e28a365674dfe8b890a205222e9b9847e841d10
Martin Weiss <albusmw@gmx.de>
2024-11-05 15:10:02 +0100

    added MyFITS project for any FITS related stuff
    FITSFixer moved to .NET 8.0

---
+++ MyFITS/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        msMain = New MenuStrip()
+        tsmiFile = New ToolStripMenuItem()
+        tsmiFile_Open = New ToolStripMenuItem()
+        tsmiOp = New ToolStripMenuItem()
+        tsmiOp_Transpose = New ToolStripMenuItem()
+        ofdMain = New OpenFileDialog()
+        tbFITSHeader = New TextBox()
+        msMain.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' msMain
+        ' 
+        msMain.Items.AddRange(New ToolStripItem() {tsmiFile, tsmiOp})
+        msMain.Location = New Point(0, 0)
+        msMain.Name = "msMain"
+        msMain.Size = New Size(1067, 24)
+        msMain.TabIndex = 0
+        msMain.Text = "MenuStrip1"
+        ' 
+        ' tsmiFile
+        ' 
+        tsmiFile.DropDownItems.AddRange(New ToolStripItem() {tsmiFile_Open})
+        tsmiFile.Name = "tsmiFile"
+        tsmiFile.Size = New Size(37, 20)
+        tsmiFile.Text = "File"
+        ' 
+        ' tsmiFile_Open
+        ' 
+        tsmiFile_Open.Name = "tsmiFile_Open"
+        tsmiFile_Open.Size = New Size(103, 22)
+        tsmiFile_Open.Text = "Open"
+        ' 
+        ' tsmiOp
+        ' 
+        tsmiOp.DropDownItems.AddRange(New ToolStripItem() {tsmiOp_Transpose})
+        tsmiOp.Name = "tsmiOp"
+        tsmiOp.Size = New Size(77, 20)
+        tsmiOp.Text = "Operations"
+        ' 
+        ' tsmiOp_Transpose
+        ' 
+        tsmiOp_Transpose.Name = "tsmiOp_Transpose"
+        tsmiOp_Transpose.Size = New Size(180, 22)
+        tsmiOp_Transpose.Text = "Transpose file"
+        ' 
+        ' tbFITSHeader
+        ' 
+        tbFITSHeader.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tbFITSHeader.Font = New Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        tbFITSHeader.Location = New Point(12, 27)
+        tbFITSHeader.Multiline = True
+        tbFITSHeader.Name = "tbFITSHeader"
+        tbFITSHeader.ScrollBars = ScrollBars.Both
+        tbFITSHeader.Size = New Size(1043, 663)
+        tbFITSHeader.TabIndex = 1
+        tbFITSHeader.WordWrap = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1067, 702)
+        Controls.Add(tbFITSHeader)
+        Controls.Add(msMain)
+        MainMenuStrip = msMain
+        Name = "Form1"
+        Text = "MyFITS"
+        msMain.ResumeLayout(False)
+        msMain.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents msMain As MenuStrip
+    Friend WithEvents tsmiFile As ToolStripMenuItem
+    Friend WithEvents tsmiFile_Open As ToolStripMenuItem
+    Friend WithEvents ofdMain As OpenFileDialog
+    Friend WithEvents tbFITSHeader As TextBox
+    Friend WithEvents tsmiOp As ToolStripMenuItem
+    Friend WithEvents tsmiOp_Transpose As ToolStripMenuItem
+
+End Class
---
+++ MyFITS/Form1.vb
@@ -0,0 +1,94 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+
+    '''<summary>Handle to Intel IPP functions.</summary>
+    Private IPP As cIntelIPP
+    Private FITS_Reader As cFITSReader
+    Private FITS_Header As cFITSHeaderParser
+    Private HeaderElements As List(Of cFITSHeaderParser.sHeaderElement)
+    Private DataStartPos As Integer = -1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load IPP
+        Dim IPPLoadError = String.Empty
+        Dim IPPPathToUse = cIntelIPP.SearchDLLToUse(IPPLoadError)
+        If String.IsNullOrEmpty(IPPLoadError) = True Then
+            IPP = New cIntelIPP(IPPPathToUse)
+        Else
+            IPP = Nothing
+        End If
+    End Sub
+
+    Private Sub tsmiFile_Open_Click(sender As Object, e As EventArgs) Handles tsmiFile_Open.Click
+        'Show open dialog
+        With ofdMain
+            .Filter = "FITS files (*.fit?)|*.fit?"
+            If .ShowDialog <> DialogResult.OK Then Exit Sub
+        End With
+        Dim LogContent As New List(Of String)
+        LogContent.Add("File : <" & ofdMain.FileName & ">")
+        'Load header and get data start position
+        HeaderElements = cFITSHeaderChanger.ParseHeader(ofdMain.FileName, DataStartPos)
+        Dim KeywordMaxLength As Integer = 0
+        Dim ValueMaxLength As Integer = cFITSHeaderChanger.MaxValueLength(HeaderElements)
+        For Each Entry As cFITSHeaderParser.sHeaderElement In HeaderElements
+            LogContent.Add(Entry.Keyword.ToString.PadRight(8, " "c) & " = " & Entry.Value.ToString.PadLeft(ValueMaxLength, " "c) & " / " & Entry.Comment.ToString.Trim)
+        Next Entry
+        LogContent.Add("Data start position : <" & DataStartPos.ValRegIndep & ">")
+        tbFITSHeader.Text = Join(LogContent.ToArray, System.Environment.NewLine)
+    End Sub
+
+    Private Sub tsmiOp_Transpose_Click(sender As Object, e As EventArgs) Handles tsmiOp_Transpose.Click
+        'Transpose the content of the file
+        Dim FixFileName As String = ofdMain.FileName & "_FIXED.fits"
+        System.IO.File.Copy(ofdMain.FileName, FixFileName)
+        Using OldFile As New System.IO.FileStream(ofdMain.FileName, IO.FileMode.Open)
+            Using NewFile As New System.IO.FileStream(FixFileName, IO.FileMode.Open)
+                'Copy header
+                Dim Header(DataStartPos - 1) As Byte
+                OldFile.Seek(0, IO.SeekOrigin.Begin)
+                OldFile.Read(Header, 0, Header.Count)
+                NewFile.Write(Header)
+                'Get dimensions
+                Dim NAXIS1 As Integer = -1
+                Dim NAXIS2 As Integer = -1
+                Dim BytePerPixel As Integer = -1
+                For Each Entry As cFITSHeaderParser.sHeaderElement In HeaderElements
+                    Select Case Entry.Keyword
+                        Case eFITSKeywords.NAXIS1
+                            NAXIS1 = CInt(Entry.Value)
+                        Case eFITSKeywords.NAXIS2
+                            NAXIS2 = CInt(Entry.Value)
+                        Case eFITSKeywords.BITPIX
+                            BytePerPixel = CInt(Math.Abs(CInt(Entry.Value)) / 8)
+                    End Select
+                Next Entry
+                'Start transpose - we assume 2 byte per value
+                'Currently not working ...
+                Dim WritePtr As Integer = -1
+                Dim SingleValueBuffer(BytePerPixel - 1) As Byte
+                For Idx1 As Integer = 0 To NAXIS1 - 1
+                    For Idx2 As Integer = 0 To NAXIS2 - 1
+                        OldFile.Read(SingleValueBuffer, 0, SingleValueBuffer.Length)
+                        WritePtr = DataStartPos + (((Idx1 * NAXIS2) + Idx2) * BytePerPixel)
+                        NewFile.Seek(WritePtr, IO.SeekOrigin.Begin)
+                        NewFile.Write(SingleValueBuffer)
+                    Next Idx2
+                Next Idx1
+                'Read to the end and just copy tail
+                Dim TailSize As Integer = CInt(OldFile.Length - OldFile.Position)
+                Dim TailSizeBuffer(TailSize - 1) As Byte
+                OldFile.Read(TailSizeBuffer, 0, TailSizeBuffer.Length)
+                NewFile.Write(TailSizeBuffer)
+                'Flush and close
+                NewFile.Flush()
+                NewFile.Close()
+            End Using
+            OldFile.Close()
+        End Using
+        MsgBox("OK")
+    End Sub
+
+End Class
