8f15e7fd7509b2d91eeb4b811c0920e236ebd50f
Martin Weiss <albusmw@gmx.de>
2024-12-10 17:58:38 +0100

    started project AstroImageConvert

---
+++ AstroImageConvert/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        tbTIFFFile = New TextBox()
+        btnProcess = New Button()
+        lbExamples = New ListBox()
+        tbLog = New TextBox()
+        SuspendLayout()
+        ' 
+        ' tbTIFFFile
+        ' 
+        tbTIFFFile.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        tbTIFFFile.Location = New Point(12, 12)
+        tbTIFFFile.Name = "tbTIFFFile"
+        tbTIFFFile.Size = New Size(1033, 23)
+        tbTIFFFile.TabIndex = 0
+        ' 
+        ' btnProcess
+        ' 
+        btnProcess.Location = New Point(12, 41)
+        btnProcess.Name = "btnProcess"
+        btnProcess.Size = New Size(75, 23)
+        btnProcess.TabIndex = 1
+        btnProcess.Text = "Process"
+        btnProcess.UseVisualStyleBackColor = True
+        ' 
+        ' lbExamples
+        ' 
+        lbExamples.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        lbExamples.FormattingEnabled = True
+        lbExamples.ItemHeight = 15
+        lbExamples.Location = New Point(12, 70)
+        lbExamples.Name = "lbExamples"
+        lbExamples.Size = New Size(1033, 184)
+        lbExamples.TabIndex = 2
+        ' 
+        ' tbLog
+        ' 
+        tbLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tbLog.Location = New Point(12, 260)
+        tbLog.Multiline = True
+        tbLog.Name = "tbLog"
+        tbLog.ScrollBars = ScrollBars.Both
+        tbLog.Size = New Size(1033, 426)
+        tbLog.TabIndex = 3
+        tbLog.WordWrap = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1057, 698)
+        Controls.Add(tbLog)
+        Controls.Add(lbExamples)
+        Controls.Add(btnProcess)
+        Controls.Add(tbTIFFFile)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents tbTIFFFile As TextBox
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents lbExamples As ListBox
+    Friend WithEvents tbLog As TextBox
+
+End Class
---
+++ AstroImageConvert/Form1.vb
@@ -0,0 +1,56 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+
+    Public Shared MyPath As String = IO.Path.GetDirectoryName(Reflection.Assembly.GetEntryAssembly.Location)
+    Dim IPP As New cIntelIPP
+    Dim DataContent(,) As Double = {}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim DD1 As New Ato.DragDrop(tbTIFFFile, True)
+        With lbExamples
+            .Items.Clear()
+            .Items.Add("C:\Users\albus\Dropbox\Transfer iPhone\downloads\libtiffpic\pc260001.tif")
+            .Items.Add("C:\Users\albus\Dropbox\Transfer iPhone\downloads\libtiffpic\quad-jpeg.tif")
+            .Items.Add("C:\Users\albus\Dropbox\Astro\!Bilder\2020-02 - Königsdorf Orion.tif")
+            .Items.Add("\\192.168.100.10\dsc\DSS_Autosave\2024_11_29 - NGC371 Ha.tif")
+            .Items.Add("\\192.168.100.10\dsc\DSS_Autosave\2024_09_13 - NGC2024_Fix16.tif")
+        End With
+    End Sub
+
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        Dim TIFF_IO As New ImageFileFormatSpecific.cTIFF
+        If System.IO.File.Exists(tbTIFFFile.Text) Then
+            tbLog.Text = "Processing ..."
+            TIFF_IO.LoadTIFF(tbTIFFFile.Text, DataContent)
+            tbLog.Text = "OK"
+        End If
+
+        Dim ImageOut(,) As Double = ImageProcessing.BinningMedian(DataContent, 4)
+        Dim ImageOut_Min As Double = ImageOut.Minimum
+        Dim ImageOut_Max As Double = ImageOut.Maximum
+        Dim FinalImage(ImageOut.GetUpperBound(0), ImageOut.GetUpperBound(1)) As UInt16
+        For Idx1 As Integer = 0 To ImageOut.GetUpperBound(0)
+            For Idx2 As Integer = 0 To ImageOut.GetUpperBound(1)
+                Dim NewValue As Double = (((ImageOut(Idx1, Idx2) - ImageOut_Min) / (ImageOut_Max - ImageOut_Min))) ^ 0.2
+                FinalImage(Idx1, Idx2) = CUShort(UInt16.MaxValue * NewValue)
+            Next Idx2
+        Next Idx1
+
+        Dim FileOut As String = System.IO.Path.Combine(MyPath, "Test.png")
+        Dim PNGSave As New ImageFileFormatSpecific.cPNG
+        PNGSave.Save_16bpp(FileOut, FinalImage)
+        MsgBox("OK")
+
+        'Dim X As New cImageFromData
+        'X.GenerateDisplayImage(DataContent, IPP)
+        'X.OutputImage.BitmapToProcess.Save("C:\Test.png")
+
+    End Sub
+
+    Private Sub lbExamples_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lbExamples.SelectedIndexChanged
+        tbTIFFFile.Text = CType(lbExamples.SelectedItem, String)
+    End Sub
+
+End Class
