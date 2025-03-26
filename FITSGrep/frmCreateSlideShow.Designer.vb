317751fafae225951a9dee668f04a6177f318862
Martin Weiss <albusmw@gmx.de>
2025-03-26 11:49:52 +0100

    FITSGrep: slide show creation added

---
+++ FITSGrep/frmCreateSlideShow.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCreateSlideShow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        pgMain = New PropertyGrid()
+        msMain = New MenuStrip()
+        tsmiFile = New ToolStripMenuItem()
+        tsmiFile_Create = New ToolStripMenuItem()
+        msMain.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' pgMain
+        ' 
+        pgMain.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        pgMain.Location = New Point(12, 27)
+        pgMain.Name = "pgMain"
+        pgMain.Size = New Size(771, 730)
+        pgMain.TabIndex = 0
+        ' 
+        ' msMain
+        ' 
+        msMain.Items.AddRange(New ToolStripItem() {tsmiFile})
+        msMain.Location = New Point(0, 0)
+        msMain.Name = "msMain"
+        msMain.Size = New Size(795, 24)
+        msMain.TabIndex = 1
+        msMain.Text = "MenuStrip1"
+        ' 
+        ' tsmiFile
+        ' 
+        tsmiFile.DropDownItems.AddRange(New ToolStripItem() {tsmiFile_Create})
+        tsmiFile.Name = "tsmiFile"
+        tsmiFile.Size = New Size(37, 20)
+        tsmiFile.Text = "File"
+        ' 
+        ' tsmiFile_Create
+        ' 
+        tsmiFile_Create.Name = "tsmiFile_Create"
+        tsmiFile_Create.Size = New Size(180, 22)
+        tsmiFile_Create.Text = "Create"
+        ' 
+        ' frmCreateSlideShow
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(795, 769)
+        Controls.Add(pgMain)
+        Controls.Add(msMain)
+        MainMenuStrip = msMain
+        Name = "frmCreateSlideShow"
+        Text = "Slide Show Creator"
+        msMain.ResumeLayout(False)
+        msMain.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents pgMain As PropertyGrid
+    Friend WithEvents msMain As MenuStrip
+    Friend WithEvents tsmiFile As ToolStripMenuItem
+    Friend WithEvents tsmiFile_Create As ToolStripMenuItem
+End Class
---
+++ FITSGrep/frmCreateSlideShow.vb
@@ -0,0 +1,22 @@
+﻿Option Explicit On
+Option Strict On
+
+#Disable Warning CA1416 ' Validate platform compatibility
+
+Public Class frmCreateSlideShow
+
+    Public SlideShow As New cCreateSlideShow
+    Public ImageFile As New List(Of String)
+
+    Private Sub frmCreateSlideShow_Load(sender As Object, e As EventArgs) Handles Me.Load
+        pgMain.SelectedObject = SlideShow
+    End Sub
+
+    Private Sub tsmiFile_Create_Click(sender As Object, e As EventArgs) Handles tsmiFile_Create.Click
+
+        SlideShow.Create(ImageFile)
+        Utils.StartWithItsEXE(SlideShow.PDFFile)
+
+    End Sub
+
+End Class
