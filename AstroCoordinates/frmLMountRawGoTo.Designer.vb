813708e74bcddd9aa13f7a550c67a1acf9366ed5
albusmw <albusmw@gmx.de>
2025-05-23 05:18:57 -0400

    updated frmLMountRawGoTo

--- AstroCoordinates/frmLMountRawGoTo.Designer.vb
+++ AstroCoordinates/frmLMountRawGoTo.Designer.vb
@@ -25,69 +25,74 @@ Partial Class frmLMountRawGoTo
         components = New ComponentModel.Container()
         btnAxis0Up = New Button()
         btnAxis0Down = New Button()
-        cbTrack = New CheckBox()
         btnAxis1Up = New Button()
         btnAxis1Down = New Button()
         lbAxis0Pos = New Label()
         lbAxis1Pos = New Label()
         gbMove = New GroupBox()
+        tlpMovement = New TableLayoutPanel()
+        Label2 = New Label()
+        Label1 = New Label()
         cbRead = New CheckBox()
         tMountRead = New Timer(components)
+        tbIncrement = New TextBox()
+        Label3 = New Label()
+        btnStartRaDecTracking = New Button()
         gbMove.SuspendLayout()
+        tlpMovement.SuspendLayout()
         SuspendLayout()
         ' 
         ' btnAxis0Up
         ' 
-        btnAxis0Up.Location = New Point(6, 22)
+        btnAxis0Up.Dock = DockStyle.Fill
+        btnAxis0Up.Font = New Font("Segoe UI", 18F)
+        btnAxis0Up.Location = New Point(3, 59)
         btnAxis0Up.Name = "btnAxis0Up"
-        btnAxis0Up.Size = New Size(59, 54)
+        btnAxis0Up.Size = New Size(131, 50)
         btnAxis0Up.TabIndex = 0
-        btnAxis0Up.Text = "Axis0 Up"
+        btnAxis0Up.Text = "▲"
         btnAxis0Up.UseVisualStyleBackColor = True
         ' 
         ' btnAxis0Down
         ' 
-        btnAxis0Down.Location = New Point(6, 128)
+        btnAxis0Down.Dock = DockStyle.Fill
+        btnAxis0Down.Font = New Font("Segoe UI", 18F)
+        btnAxis0Down.Location = New Point(3, 171)
         btnAxis0Down.Name = "btnAxis0Down"
-        btnAxis0Down.Size = New Size(59, 54)
+        btnAxis0Down.Size = New Size(131, 50)
         btnAxis0Down.TabIndex = 1
-        btnAxis0Down.Text = "Axis0 Up"
+        btnAxis0Down.Text = "▼"
         btnAxis0Down.UseVisualStyleBackColor = True
-        ' 
-        ' cbTrack
-        ' 
-        cbTrack.AutoSize = True
-        cbTrack.Location = New Point(12, 243)
-        cbTrack.Name = "cbTrack"
-        cbTrack.Size = New Size(54, 19)
-        cbTrack.TabIndex = 2
-        cbTrack.Text = "Track"
-        cbTrack.UseVisualStyleBackColor = True
         ' 
         ' btnAxis1Up
         ' 
-        btnAxis1Up.Location = New Point(81, 22)
+        btnAxis1Up.Dock = DockStyle.Fill
+        btnAxis1Up.Font = New Font("Segoe UI", 18F)
+        btnAxis1Up.Location = New Point(140, 59)
         btnAxis1Up.Name = "btnAxis1Up"
-        btnAxis1Up.Size = New Size(59, 54)
+        btnAxis1Up.Size = New Size(131, 50)
         btnAxis1Up.TabIndex = 3
-        btnAxis1Up.Text = "Axis1 Up"
+        btnAxis1Up.Text = "▲"
         btnAxis1Up.UseVisualStyleBackColor = True
         ' 
         ' btnAxis1Down
         ' 
-        btnAxis1Down.Location = New Point(81, 128)
+        btnAxis1Down.Dock = DockStyle.Fill
+        btnAxis1Down.Font = New Font("Segoe UI", 18F)
+        btnAxis1Down.Location = New Point(140, 171)
         btnAxis1Down.Name = "btnAxis1Down"
-        btnAxis1Down.Size = New Size(59, 54)
+        btnAxis1Down.Size = New Size(131, 50)
         btnAxis1Down.TabIndex = 4
-        btnAxis1Down.Text = "Axis1 Down"
+        btnAxis1Down.Text = "▼"
         btnAxis1Down.UseVisualStyleBackColor = True
         ' 
         ' lbAxis0Pos
         ' 
         lbAxis0Pos.BorderStyle = BorderStyle.FixedSingle
-        lbAxis0Pos.Location = New Point(6, 77)
+        lbAxis0Pos.Dock = DockStyle.Fill
+        lbAxis0Pos.Location = New Point(3, 112)
         lbAxis0Pos.Name = "lbAxis0Pos"
-        lbAxis0Pos.Size = New Size(57, 51)
+        lbAxis0Pos.Size = New Size(131, 56)
         lbAxis0Pos.TabIndex = 5
         lbAxis0Pos.Text = "???"
         lbAxis0Pos.TextAlign = ContentAlignment.MiddleCenter
@@ -95,27 +100,70 @@ Partial Class frmLMountRawGoTo
         ' lbAxis1Pos
         ' 
         lbAxis1Pos.BorderStyle = BorderStyle.FixedSingle
-        lbAxis1Pos.Location = New Point(81, 77)
+        lbAxis1Pos.Dock = DockStyle.Fill
+        lbAxis1Pos.Location = New Point(140, 112)
         lbAxis1Pos.Name = "lbAxis1Pos"
-        lbAxis1Pos.Size = New Size(57, 51)
+        lbAxis1Pos.Size = New Size(131, 56)
         lbAxis1Pos.TabIndex = 6
         lbAxis1Pos.Text = "???"
         lbAxis1Pos.TextAlign = ContentAlignment.MiddleCenter
         ' 
         ' gbMove
         ' 
-        gbMove.Controls.Add(btnAxis0Up)
-        gbMove.Controls.Add(lbAxis1Pos)
-        gbMove.Controls.Add(btnAxis0Down)
-        gbMove.Controls.Add(lbAxis0Pos)
-        gbMove.Controls.Add(btnAxis1Up)
-        gbMove.Controls.Add(btnAxis1Down)
+        gbMove.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        gbMove.Controls.Add(tlpMovement)
         gbMove.Location = New Point(12, 43)
         gbMove.Name = "gbMove"
-        gbMove.Size = New Size(150, 194)
+        gbMove.Size = New Size(286, 254)
         gbMove.TabIndex = 7
         gbMove.TabStop = False
         gbMove.Text = "Movement"
+        ' 
+        ' tlpMovement
+        ' 
+        tlpMovement.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tlpMovement.ColumnCount = 2
+        tlpMovement.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        tlpMovement.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        tlpMovement.Controls.Add(Label2, 1, 0)
+        tlpMovement.Controls.Add(Label1, 0, 0)
+        tlpMovement.Controls.Add(btnAxis0Down, 0, 3)
+        tlpMovement.Controls.Add(btnAxis0Up, 0, 1)
+        tlpMovement.Controls.Add(btnAxis1Down, 1, 3)
+        tlpMovement.Controls.Add(lbAxis0Pos, 0, 2)
+        tlpMovement.Controls.Add(lbAxis1Pos, 1, 2)
+        tlpMovement.Controls.Add(btnAxis1Up, 1, 1)
+        tlpMovement.Location = New Point(6, 24)
+        tlpMovement.Name = "tlpMovement"
+        tlpMovement.RowCount = 4
+        tlpMovement.RowStyles.Add(New RowStyle(SizeType.Percent, 25F))
+        tlpMovement.RowStyles.Add(New RowStyle(SizeType.Percent, 25F))
+        tlpMovement.RowStyles.Add(New RowStyle(SizeType.Percent, 25F))
+        tlpMovement.RowStyles.Add(New RowStyle(SizeType.Percent, 25F))
+        tlpMovement.Size = New Size(274, 224)
+        tlpMovement.TabIndex = 9
+        ' 
+        ' Label2
+        ' 
+        Label2.BorderStyle = BorderStyle.FixedSingle
+        Label2.Dock = DockStyle.Fill
+        Label2.Location = New Point(140, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(131, 56)
+        Label2.TabIndex = 8
+        Label2.Text = "Axis1"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label1
+        ' 
+        Label1.BorderStyle = BorderStyle.FixedSingle
+        Label1.Dock = DockStyle.Fill
+        Label1.Location = New Point(3, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(131, 56)
+        Label1.TabIndex = 7
+        Label1.Text = "Axis0"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
         ' 
         ' cbRead
         ' 
@@ -130,24 +178,55 @@ Partial Class frmLMountRawGoTo
         ' tMountRead
         ' 
         ' 
+        ' tbIncrement
+        ' 
+        tbIncrement.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        tbIncrement.Location = New Point(235, 10)
+        tbIncrement.Name = "tbIncrement"
+        tbIncrement.Size = New Size(57, 23)
+        tbIncrement.TabIndex = 9
+        tbIncrement.Text = "5"
+        tbIncrement.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(136, 13)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "Increment [°]:"
+        ' 
+        ' btnStartRaDecTracking
+        ' 
+        btnStartRaDecTracking.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnStartRaDecTracking.Location = New Point(12, 303)
+        btnStartRaDecTracking.Name = "btnStartRaDecTracking"
+        btnStartRaDecTracking.Size = New Size(286, 30)
+        btnStartRaDecTracking.TabIndex = 11
+        btnStartRaDecTracking.Text = "Start RaDec tracking from here"
+        btnStartRaDecTracking.UseVisualStyleBackColor = True
+        ' 
         ' frmLMountRawGoTo
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(172, 275)
+        ClientSize = New Size(310, 345)
+        Controls.Add(btnStartRaDecTracking)
+        Controls.Add(Label3)
+        Controls.Add(tbIncrement)
         Controls.Add(cbRead)
         Controls.Add(gbMove)
-        Controls.Add(cbTrack)
         Name = "frmLMountRawGoTo"
         Text = "L Mount Raw GoTo"
         gbMove.ResumeLayout(False)
+        tlpMovement.ResumeLayout(False)
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents btnAxis0Up As Button
     Friend WithEvents btnAxis0Down As Button
-    Friend WithEvents cbTrack As CheckBox
     Friend WithEvents btnAxis1Up As Button
     Friend WithEvents btnAxis1Down As Button
     Friend WithEvents lbAxis0Pos As Label
@@ -155,4 +234,10 @@ Partial Class frmLMountRawGoTo
     Friend WithEvents gbMove As GroupBox
     Friend WithEvents cbRead As CheckBox
     Friend WithEvents tMountRead As Timer
+    Friend WithEvents tlpMovement As TableLayoutPanel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbIncrement As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnStartRaDecTracking As Button
 End Class
--- AstroCoordinates/frmLMountRawGoTo.vb
+++ AstroCoordinates/frmLMountRawGoTo.vb
@@ -1,18 +1,72 @@
 ﻿Option Explicit On
 Option Strict On
+Imports System.Linq.Expressions
 
 Public Class frmLMountRawGoTo
+
+    Dim Axis0 As Double = Double.NaN
+    Dim Axis1 As Double = Double.NaN
+
+    Private ReadOnly Property Increment() As Double
+        Get
+            Return tbIncrement.Text.ValRegIndep
+        End Get
+    End Property
 
     Private Sub cbRead_CheckedChanged(sender As Object, e As EventArgs) Handles cbRead.CheckedChanged
         tMountRead.Enabled = cbRead.Checked
     End Sub
 
+    Private Sub btnAxis0Up_Click(sender As Object, e As EventArgs) Handles btnAxis0Up.Click
+        UpdateAxisPos()
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(Axis0 + Increment, Axis1))
+    End Sub
+
+    Private Sub btnAxis0Down_Click(sender As Object, e As EventArgs) Handles btnAxis0Down.Click
+        UpdateAxisPos()
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(Axis0 - Increment, Axis1))
+    End Sub
+
+    Private Sub btnAxis1Up_Click(sender As Object, e As EventArgs) Handles btnAxis1Up.Click
+        UpdateAxisPos()
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(Axis0, Axis1 + Increment))
+    End Sub
+
+    Private Sub btnAxis1Down_Click(sender As Object, e As EventArgs) Handles btnAxis1Down.Click
+        UpdateAxisPos()
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(Axis0, Axis1 - Increment))
+    End Sub
+
     Private Sub tMountRead_Tick(sender As Object, e As EventArgs) Handles tMountRead.Tick
+        UpdateAxisPos()
+    End Sub
+
+    Private Sub UpdateAxisPos()
         DB.PWI4.LoadStatus()
-        Dim Axis0 As Double = CType(DB.PWI4.GetValue(ePWI4.mount__axis0__position_degs), Double)
-        Dim Axis1 As Double = CType(DB.PWI4.GetValue(ePWI4.mount__axis1__position_degs), Double)
-        lbAxis0Pos.Text = Axis0.ValRegIndep("000.0000")
-        lbAxis1Pos.Text = Axis1.ValRegIndep("000.0000")
+        Axis0 = CType(DB.PWI4.GetValue(ePWI4.mount__axis0__position_degs), Double)
+        Axis1 = CType(DB.PWI4.GetValue(ePWI4.mount__axis1__position_degs), Double)
+        lbAxis0Pos.Text = Axis0.ValRegIndep("000.000000")
+        lbAxis1Pos.Text = Axis1.ValRegIndep("000.000000")
+    End Sub
+
+    Private Sub lbAxis0Pos_Click(sender As Object, e As EventArgs) Handles lbAxis0Pos.Click
+        DB.PWI4.LoadStatus()
+        Dim NewPos As String = InputBox("Axis0 Goto ...", "Axis0 GOTO", lbAxis0Pos.Text)
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(NewPos.ValRegIndep, Axis1))
+    End Sub
+
+    Private Sub lbAxis1Pos_Click(sender As Object, e As EventArgs) Handles lbAxis1Pos.Click
+        DB.PWI4.LoadStatus()
+        Dim NewPos As String = InputBox("Axis1 Goto ...", "Axis1 GOTO", lbAxis1Pos.Text)
+        Dim Response As String = Download.GetResponse(DB.PWI4.Command.Goto_Raw(Axis0, NewPos.ValRegIndep))
+    End Sub
+
+    Private Sub btnStartRaDecTracking_Click(sender As Object, e As EventArgs) Handles btnStartRaDecTracking.Click
+        DB.PWI4.LoadStatus()
+        Dim Ra As Double = CType(DB.PWI4.GetValue(ePWI4.mount__ra_j2000_hours), Double)
+        Dim Dec As Double = CType(DB.PWI4.GetValue(ePWI4.mount__dec_j2000_degs), Double)
+        Download.GetResponse(DB.PWI4.Command.Goto_RaDec(True, Ra, Dec))
+        Download.GetResponse(DB.PWI4.Command.Tracking(True))
     End Sub
 
 End Class
