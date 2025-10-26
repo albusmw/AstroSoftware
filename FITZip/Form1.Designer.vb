a47975a04822ac0a0ef844ceba69861a91e9e4b6
albusmw <albusmw@gmx.de>
2025-10-26 14:56:14 +0100

    AstroCMD removed and create a new project FITZip (as AstroCMD was only the zipper)

---
+++ FITZip/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lbFiles = New ListBox()
+        btnCompress = New Button()
+        ssMain = New StatusStrip()
+        tsslStatus = New ToolStripStatusLabel()
+        tbLog = New TextBox()
+        scMain = New SplitContainer()
+        ssMain.SuspendLayout()
+        CType(scMain, ComponentModel.ISupportInitialize).BeginInit()
+        scMain.Panel1.SuspendLayout()
+        scMain.Panel2.SuspendLayout()
+        scMain.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' lbFiles
+        ' 
+        lbFiles.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        lbFiles.Font = New Font("Courier New", 9.75F)
+        lbFiles.FormattingEnabled = True
+        lbFiles.IntegralHeight = False
+        lbFiles.Location = New Point(3, 3)
+        lbFiles.Name = "lbFiles"
+        lbFiles.Size = New Size(1094, 282)
+        lbFiles.TabIndex = 0
+        ' 
+        ' btnCompress
+        ' 
+        btnCompress.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        btnCompress.Location = New Point(12, 595)
+        btnCompress.Name = "btnCompress"
+        btnCompress.Size = New Size(111, 45)
+        btnCompress.TabIndex = 1
+        btnCompress.Text = "Compress"
+        btnCompress.UseVisualStyleBackColor = True
+        ' 
+        ' ssMain
+        ' 
+        ssMain.Items.AddRange(New ToolStripItem() {tsslStatus})
+        ssMain.Location = New Point(0, 643)
+        ssMain.Name = "ssMain"
+        ssMain.Size = New Size(1124, 22)
+        ssMain.TabIndex = 2
+        ssMain.Text = "StatusStrip1"
+        ' 
+        ' tsslStatus
+        ' 
+        tsslStatus.Name = "tsslStatus"
+        tsslStatus.Size = New Size(30, 17)
+        tsslStatus.Text = "IDLE"
+        ' 
+        ' tbLog
+        ' 
+        tbLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        tbLog.Font = New Font("Courier New", 9.75F)
+        tbLog.Location = New Point(3, 3)
+        tbLog.Multiline = True
+        tbLog.Name = "tbLog"
+        tbLog.ScrollBars = ScrollBars.Both
+        tbLog.Size = New Size(1094, 279)
+        tbLog.TabIndex = 3
+        tbLog.WordWrap = False
+        ' 
+        ' scMain
+        ' 
+        scMain.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        scMain.Location = New Point(12, 12)
+        scMain.Name = "scMain"
+        scMain.Orientation = Orientation.Horizontal
+        ' 
+        ' scMain.Panel1
+        ' 
+        scMain.Panel1.Controls.Add(lbFiles)
+        ' 
+        ' scMain.Panel2
+        ' 
+        scMain.Panel2.Controls.Add(tbLog)
+        scMain.Size = New Size(1100, 577)
+        scMain.SplitterDistance = 288
+        scMain.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1124, 665)
+        Controls.Add(scMain)
+        Controls.Add(ssMain)
+        Controls.Add(btnCompress)
+        Name = "Form1"
+        Text = "FITZip - ZIP FITS files"
+        ssMain.ResumeLayout(False)
+        ssMain.PerformLayout()
+        scMain.Panel1.ResumeLayout(False)
+        scMain.Panel2.ResumeLayout(False)
+        scMain.Panel2.PerformLayout()
+        CType(scMain, ComponentModel.ISupportInitialize).EndInit()
+        scMain.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lbFiles As ListBox
+    Friend WithEvents btnCompress As Button
+    Friend WithEvents ssMain As StatusStrip
+    Friend WithEvents tsslStatus As ToolStripStatusLabel
+    Friend WithEvents tbLog As TextBox
+    Friend WithEvents scMain As SplitContainer
+
+End Class
---
+++ FITZip/Form1.vb
@@ -0,0 +1,253 @@
+﻿Option Explicit On
+Option Strict On
+
+'''<summary>Code and decode a file using Shannon Fano coding.</summary>
+Public Class Form1
+
+    'Test files:
+    ' "C:\Users\albus\OneDrive\Transfer_Kevin_Morefield\QHY600_L_300_025_020_003_060_ExtendFullwell.fits"
+    ' "C:\!Work\TestData\QHY600_H_alpha_480_056_050_001_060_Photographic.fits"
+    ' "C:\!Work\TestData\NGC7293 (Helix nebula)_00002.fits"
+    ' "C:\!Work\TestData\ShanFano1.fits"
+    ' "\\192.168.100.10\dsc\2025_03_31 - Flat OIII\Flat OIII_00011.fits"
+
+    Public Class cFileProps
+        '''<summary>Name of the file.</summary>
+        Public FileName As String = String.Empty
+        '''<summary>Data start position of the FITS file.</summary>
+        Public DataStartPos As Integer = -1
+        '''<summary>Checksum.</summary>
+        Public Checksum As String = String.Empty
+        '''<summary>Name of the output file.</summary>
+        Public OutFile As String = String.Empty
+        '''<summary>Raw header.</summary>
+        Public RawHeader As Byte() = {}
+        '''<summary>LOG content.</summary>
+        Public AllLog As New List(Of String)
+    End Class
+
+    '''<summary>Location of the EXE.</summary>
+    Public ReadOnly Property MyPath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly.Location)
+
+    '''<summary>Drag-and-drop handler.</summary>
+    Private WithEvents DD As cDragDrop
+
+    '''<summary>Use the IPP.</summary>
+    Private UseIPP As Boolean = True
+
+    '''<summary>Read FITS data as-is without using BZERO or BSCALE.</summary>
+    Private ForceDirect As Boolean = True
+
+    '''<summary>IPP path to use.</summary>
+    Private IPPPathToUse As String = String.Empty
+
+    '''<summary>Handle to Intel IPP functions.</summary>
+    Private IPP As cIntelIPP = Nothing
+
+    '''<summary>FITS reader.</summary>
+    Private FITSReader As cFITSReader = Nothing
+
+    '''<summary>FITS header parser.</summary>
+    Private FITSHeaderParser As cFITSHeaderParser = Nothing
+
+    '''<summary>Statistics calculator (where the image data are stored ...).</summary>
+    Private SingleStatCalc As AstroNET.Statistics = Nothing
+
+    '''<summary>Block size for FITS files.</summary>
+    Private Const FITSBlockSize As Integer = 2880
+
+    '''<summary>Image statistics.</summary>
+    Private Statistics As AstroNET.Statistics.sStatistics
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        'Load IPP
+        Dim IPPLoadError = String.Empty
+        IPPPathToUse = cIntelIPP.SearchDLLToUse(cIntelIPP.PossiblePaths(MyPath).ToArray, IPPLoadError)
+        If String.IsNullOrEmpty(IPPLoadError) = False Then
+            MsgBox("IPP not found <" & IPPLoadError & ">")
+        End If
+
+        'Init drap-and-drop
+        DD = New cDragDrop(lbFiles, True)
+
+    End Sub
+
+    Private Sub btnCompress_Click(sender As Object, e As EventArgs) Handles btnCompress.Click
+        For Each File As String In lbFiles.Items
+            Dim FileProps As New cFileProps
+            FileProps.FileName = File
+            Compress(FileProps)
+        Next File
+    End Sub
+
+    Private Function Compress(ByRef FileProps As cFileProps) As Boolean
+
+        Dim Stopper As New Stopwatch
+
+        'Create IPP instance - search path automatically
+        If IsNothing(IPP) Then IPP = New cIntelIPP
+        If IsNothing(FITSReader) Then FITSReader = New cFITSReader(IPP.IPPPath)
+        If IsNothing(SingleStatCalc) Then SingleStatCalc = New AstroNET.Statistics(IPP)
+
+        'Start with file
+        Logging(FileProps, "FILE <" & FileProps.FileName & ">")
+
+        'Read fits header
+        Stopper.Restart() : Stopper.Start()
+        Logging(FileProps, "  Read header ...")
+        FITSHeaderParser = New cFITSHeaderParser(cFITSHeaderChanger.ParseHeader(FileProps.FileName, FileProps.DataStartPos))
+        Logging(FileProps, "    -> data format: <" & FITSHeaderParser.BitPix.ValRegIndep & ">")
+        Finish(FileProps, Stopper)
+
+        'Get raw header bytes
+        Using X As IO.FileStream = System.IO.File.OpenRead(FileProps.FileName)
+            ReDim FileProps.RawHeader(FileProps.DataStartPos - 1)
+            X.Seek(0, IO.SeekOrigin.Begin)
+            X.Read(FileProps.RawHeader, 0, FileProps.RawHeader.Length)
+            X.Close()
+        End Using
+
+        'Check file conditions
+        Stopper.Restart() : Stopper.Start()
+        Logging(FileProps, "  File consistance check ...")
+        Dim FileOK As Boolean = True
+        If FITSHeaderParser.BitPix <> 16 Then
+            Logging(FileProps, "-- ERROR: Only BitPix format 16 supported --")
+            FileOK = False
+        End If
+        Dim CheckResult As String = CheckFITSFileIsOK(FileProps)
+        If String.IsNullOrEmpty(CheckResult) = False Then
+            Logging(FileProps, "-- ERROR: " & CheckResult)
+            FileOK = False
+        End If
+
+        'Set the output file
+        If String.IsNullOrEmpty(FileProps.OutFile) Then
+            FileProps.OutFile = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(FileProps.FileName), System.IO.Path.GetFileNameWithoutExtension(FileProps.FileName) & ".tar")
+        End If
+        If System.IO.File.Exists(FileProps.OutFile) Then
+            Logging(FileProps, "-- ERROR: Output file <" & FileProps.OutFile & "> already exists")
+            FileOK = False
+        End If
+        Finish(FileProps, Stopper)
+
+        If FileOK Then
+
+            'Get checksum of complete file
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Get checksum ...")
+            FileProps.Checksum = RecursiveDirScanner.GetHash(FileProps.FileName)
+            Logging(FileProps, "    -> Checksum: <" & FileProps.Checksum & ">")
+            Finish(FileProps, Stopper)
+
+            'Read FITS data
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Read FITS data ...")
+            SingleStatCalc.ResetAllProcessors()
+            With SingleStatCalc.DataProcessor_UInt16
+                .ImageData(0).Data = FITSReader.ReadInUInt16(FileProps.FileName, UseIPP, ForceDirect)
+                If FITSHeaderParser.NAXIS3 > 1 Then
+                    For Idx As Integer = 1 To FITSHeaderParser.NAXIS3 - 1
+                        FileProps.DataStartPos += CInt(.ImageData(Idx - 1).Length * FITSHeaderParser.BytesPerSample)        'move to next plane
+                        .ImageData(Idx).Data = FITSReader.ReadInUInt16(FileProps.FileName, FileProps.DataStartPos, UseIPP, ForceDirect)
+                    Next Idx
+                End If
+            End With
+            Finish(FileProps, Stopper)
+
+            'Calculate statistics
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Calculate statistics ...")
+            Statistics = SingleStatCalc.ImageStatistics()
+            Finish(FileProps, Stopper)
+
+            'Code book generation
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Codebook generation ...")
+            Dim Dic As Dictionary(Of UInt16, ULong) = Statistics.MonochromHistogram_Uint16
+            Dim SFGen As New cShanFano(Of UInt16)
+            Logging(FileProps, "    ", SFGen.GenCodeBook(Dic))
+            Finish(FileProps, Stopper)
+
+            'Compression
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Compression ...")
+            SFGen.Compress(SingleStatCalc.DataProcessor_UInt16.ImageData(0).Data)
+            Finish(FileProps, Stopper)
+
+            'Store
+            Stopper.Restart() : Stopper.Start()
+            Logging(FileProps, "  Storing ...")
+            SFGen.StoreSFTar(FileProps.OutFile, FileProps.RawHeader, FileProps.Checksum)
+            Finish(FileProps, Stopper)
+
+            'Normal compression (for reference)
+            If 1 = 0 Then
+                Stopper.Restart() : Stopper.Start()
+                Logging(FileProps, "Normal compression comparison")
+                Logging(FileProps, SFGen.CompressStandard(FileProps.FileName))
+                Finish(FileProps, Stopper)
+            End If
+
+            Logging(FileProps, "END.")
+
+            Logging(FileProps, "══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════")
+            Return True
+
+        Else
+
+            Logging(FileProps, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            Return False
+
+        End If
+
+    End Function
+
+    Private Function CheckFITSFileIsOK(ByRef FileProps As cFileProps) As String
+        Dim FileSize As Long = (New System.IO.FileInfo(FileProps.FileName)).Length
+        Dim HeaderLength As Long = FileProps.DataStartPos
+        Dim DataContentLength As Long = (FITSHeaderParser.Width * FITSHeaderParser.Height * 2)
+        Dim TailLength As Long = FileSize - HeaderLength - DataContentLength
+        If TailLength = 0 Then
+            Return String.Empty
+        Else
+            'Check if the rest of the bytes is 0
+            Using Checker As New System.IO.FileStream(FileProps.FileName, IO.FileMode.Open, IO.FileAccess.Read)
+                Dim Tail(CInt(TailLength) - 1) As Byte
+                Checker.Seek(HeaderLength + DataContentLength, IO.SeekOrigin.Begin)
+                Checker.Read(Tail, 0, Tail.Length)
+                For Each SingleByte As Byte In Tail
+                    If SingleByte <> 0 Then
+                        Return "Non-zero tail byte found"
+                    End If
+                Next SingleByte
+            End Using
+            Return String.Empty
+        End If
+    End Function
+
+    Private Sub Finish(ByRef FileProps As cFileProps, ByVal Stopper As Stopwatch)
+        Stopper.Stop()
+        Logging(FileProps, "    DONE in <" & Stopper.ElapsedMilliseconds.ValRegIndep & " ms>")
+        Logging(FileProps, "──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────")
+    End Sub
+
+    Private Sub Logging(ByRef FileProps As cFileProps, ByVal LogText As String)
+        Logging(FileProps, New String() {LogText})
+    End Sub
+
+    Private Sub Logging(ByRef FileProps As cFileProps, ByVal Indent As String, ByVal LogText() As String)
+        For Each Line As String In LogText
+            Logging(FileProps, Indent & Line)
+        Next Line
+    End Sub
+
+    Private Sub Logging(ByRef FileProps As cFileProps, ByVal LogText() As String)
+        FileProps.AllLog.AddRange(LogText)
+        tbLog.Text = Join(FileProps.AllLog.ToArray, System.Environment.NewLine)
+        tbLog.SelectionStart = tbLog.Text.Length - 1 : tbLog.ScrollToCaret()
+        System.Windows.Forms.Application.DoEvents()
+    End Sub
+
+End Class
